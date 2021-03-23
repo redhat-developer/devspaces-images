5275f3eb49aff3dd7874d9d5a90d3611d193406f
Kirk Crane <kcrane@redhat.com>
2021-03-23 11:53:15 -0400

    CRW-1678 ensure -p and -n flags on all get-sources-jenkins.sh
    Signed-off-by: Kirk Crane <kcrane@redhat.com>

--- codeready-workspaces-stacks-dotnet/get-sources-jenkins.sh
+++ codeready-workspaces-stacks-dotnet/get-sources-jenkins.sh
@@ -4,6 +4,7 @@
 scratchFlag=""
 while [[ "$#" -gt 0 ]]; do
   case $1 in
+    '-p'|'--force-pull') shift 0;;
     '-n'|'--nobuild') exit 0; shift 0;;
     '-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
   esac
