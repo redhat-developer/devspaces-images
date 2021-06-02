fc61249035e934b7e715f6a3e6928c85a5fdd87b
Josh Pinkney <joshpinkney@gmail.com>
2021-06-02 16:40:14 -0400

    fix: don't use yarn install, instead just use the node_modules that are present
    Signed-off-by: Josh Pinkney <joshpinkney@gmail.com>

--- codeready-workspaces-dashboard/build/scripts/sync.sh
+++ codeready-workspaces-dashboard/build/scripts/sync.sh
@@ -106,7 +106,7 @@ sed ${TARGETDIR}/build/dockerfiles/rhel.Dockerfile -r \
     -e 's|FROM registry.access.redhat.com/|FROM |' \
     -e 's|FROM registry.redhat.io/|FROM |' \
     `# insert logic to unpack asset-node-modules-cache.tgz into /dashboard/node-modules` \
-    -e "/RUN \/dashboard\/.yarn\/releases\/yarn-\*.cjs install/i COPY asset-node-modules-cache.tgz /tmp/\nRUN tar xzf /tmp/asset-node-modules-cache.tgz && rm -f /tmp/asset-node-modules-cache.tgz" \
+    -e "/RUN \/dashboard\/.yarn\/releases\/yarn-\*.cjs install/c COPY asset-node-modules-cache.tgz /tmp/\nRUN tar xzf /tmp/asset-node-modules-cache.tgz && rm -f /tmp/asset-node-modules-cache.tgz" \
 > ${TARGETDIR}/Dockerfile
 cat << EOT >> ${TARGETDIR}/Dockerfile
 ENV SUMMARY="Red Hat CodeReady Workspaces dashboard container" \\
