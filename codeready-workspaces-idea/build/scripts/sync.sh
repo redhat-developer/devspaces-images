5430c47e7c0445eb513507940b18ac5410db2a56
Vladyslav Zhukovskyi <vzhukovs@redhat.com>
2021-07-28 21:49:25 +0300

    Change asset's names according to brew conventions (#70)
    Signed-off-by: Vladyslav Zhukovskyi <vzhukovs@redhat.com>

--- codeready-workspaces-idea/build/scripts/sync.sh
+++ codeready-workspaces-idea/build/scripts/sync.sh
@@ -90,10 +90,6 @@ sed_in_place -r \
   -e "s#FROM (registry.access.redhat.com|registry.redhat.io)/#FROM #g" \
   `# Remove unused Python packages (support for PyCharm not included in CRW)` \
   -e "/# Python support/d" -e "/python2 python39 \\\\/d" \
-  `# use CRW filename conventions - tarballs should start with asset- and end with a filetype` \
-  -e "s#ide-packaging#asset-ide-packaging.tgz#" \
-  -e "s#projector-server-assembly#asset-projector-server-assembly.tgz#" \
-  -e "s#static-assembly#asset-static-assembly.tgz#" \
   "${TARGETDIR}"/Dockerfile
 
 # Overwrite default configuration
