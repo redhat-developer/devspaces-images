e3cf09f2646dfb09e9140d388e7c37e16d779235
nickboldt <nboldt@redhat.com>
2021-08-23 14:33:10 -0300

    CRW-2081 CRW-1621 create new sync.sh script; instead of transforming exp.Dockerfile to Dockerfile, keep a rhel.container.Dockerfile copy we can use; add rhel.binary.Dockerfile too
    Change-Id: I013bc0c2764382d03925eafd8d5c5686a9f84815
    Signed-off-by: nickboldt <nboldt@redhat.com>
    
    CRW-2188 include GOLANG_VERSION and TRAEFIK_VERSION in container build; include GOLANG_VERSION and TRAEFIK_SHA in binary build
    
    Change-Id: Id50e28ccd26f6ddd8a80bd96f961409b2c095867
    Signed-off-by: nickboldt <nboldt@redhat.com>

---
+++ codeready-workspaces-traefik/build/scripts/sync.sh
@@ -0,0 +1,126 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# convert upstream repo to midstream crw-images repo using yq, sed
+
+set -e
+
+# defaults
+CSV_VERSION=2.y.0 # csv 2.y.0
+CRW_VERSION=${CSV_VERSION%.*} # tag 2.y
+TRAEFIK_VERSION="v2.5.0"
+GOLANG_VERSION="1.16.2"
+UPSTM_NAME="traefik"
+MIDSTM_NAME="traefik"
+
+usage () {
+    echo "
+Usage:   $0 -v [CRW CSV_VERSION] [-s /path/to/${UPSTM_NAME}] [-t /path/to/generated]
+Example: $0 -v 2.y.0 -s ${HOME}/projects/${UPSTM_NAME} -t /tmp/crw-${MIDSTM_NAME}"
+    exit
+}
+
+if [[ $# -lt 6 ]]; then usage; fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-v') CSV_VERSION="$2"; CRW_VERSION="${CSV_VERSION%.*}"; shift 1;;
+    # paths to use for input and output
+    '-s') SOURCEDIR="$2"; SOURCEDIR="${SOURCEDIR%/}"; shift 1;;
+    '-t') TARGETDIR="$2"; TARGETDIR="${TARGETDIR%/}"; shift 1;;
+    # special params for this sync
+    '--traefik-version') TRAEFIK_VERSION="$2"; shift 1;;
+    '--golang-version') GOLANG_VERSION="$2"; shift 1;;
+    '--help'|'-h') usage;;
+  esac
+  shift 1
+done
+
+if [[ ! -d "${SOURCEDIR}" ]]; then usage; fi
+if [[ ! -d "${TARGETDIR}" ]]; then usage; fi
+if [[ "${CSV_VERSION}" == "2.y.0" ]]; then usage; fi
+
+# ignore changes in these files
+echo ".github/
+.git/
+.gitignore
+.dockerignore
+.semaphoreci
+.travis
+.travis.yaml
+.gitattributes
+traefik.sample.toml
+traefik.sample.yml
+build/
+tests/
+/container.yaml
+/content_sets.*
+/cvp.yml
+/patches/README.md
+README.md
+get-source*.sh
+tests/basic-test.yaml
+sources
+make-release.sh
+rhel.Dockerfile
+" > /tmp/rsync-excludes
+echo "Rsync ${SOURCEDIR} to ${TARGETDIR}"
+rsync -azrlt --checksum --exclude-from /tmp/rsync-excludes --delete "${SOURCEDIR}"/ "${TARGETDIR}"/
+rm -f /tmp/rsync-excludes
+
+# ensure shell scripts are executable
+find "${TARGETDIR}"/ -name "*.sh" -exec chmod +x {} \;
+
+sed_in_place() {
+    SHORT_UNAME=$(uname -s)
+  if [ "$(uname)" == "Darwin" ]; then
+    sed -i '' "$@"
+  elif [ "${SHORT_UNAME:0:5}" == "Linux" ]; then
+    sed -i "$@"
+  fi
+}
+
+SOURCE_SHA=$(cd "${SOURCEDIR}"; git checkout "${TRAEFIK_VERSION}"; git rev-parse --short=4 HEAD || true)
+sed_in_place -r \
+  `# Use the current SHA & correct GOLANG version to build` \
+  -e "s#ARG GOLANG_VERSION=\".*\"#ARG GOLANG_VERSION=\"${GOLANG_VERSION}\"#g" \
+  -e "s#ARG TRAEFIK_SHA=\".*\"#ARG TRAEFIK_SHA=\"${SOURCE_SHA}\"#g" \
+  "${TARGETDIR}"/build/rhel.binary.Dockerfile
+
+sed -r \
+  `# Remove registry so build works in Brew` \
+  -e "s#FROM (registry.access.redhat.com|registry.redhat.io)/#FROM #g" \
+  "${TARGETDIR}"/build/rhel.container.Dockerfile > "${TARGETDIR}"/Dockerfile
+
+cat << EOT >> "${TARGETDIR}"/Dockerfile
+
+ENV SUMMARY="Red Hat CodeReady Workspaces ${MIDSTM_NAME} container" \\
+    DESCRIPTION="Red Hat CodeReady Workspaces ${MIDSTM_NAME} container" \\
+    PRODNAME="codeready-workspaces" \\
+    COMPNAME="${MIDSTM_NAME}-rhel8" \\
+    TRAEFIK_VERSION="${TRAEFIK_VERSION}" \\
+    GOLANG_VERSION="${GOLANG_VERSION}"
+
+LABEL summary="\$SUMMARY" \\
+      description="\$DESCRIPTION" \\
+      io.k8s.description="\$DESCRIPTION" \\
+      io.k8s.display-name="\$DESCRIPTION" \\
+      io.openshift.tags="\$PRODNAME,\$COMPNAME" \\
+      com.redhat.component="\$PRODNAME-\$COMPNAME-container" \\
+      name="\$PRODNAME/\$COMPNAME" \\
+      version="${CRW_VERSION}" \\
+      license="EPLv2" \\
+      maintainer="Lukas Krejci <lkrejci@redhat.com>, Nick Boldt <nboldt@redhat.com>" \\
+      io.openshift.expose-services="" \\
+      usage=""
+EOT
+echo "Converted Dockerfile"
