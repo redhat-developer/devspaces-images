8d24a1a839a6302991bf99fc2ec6288999f5ca5a
SDawley <88796121+SDawley@users.noreply.github.com>
2021-10-13 11:50:50 -0400

    Update rhel.Dockerfile.extract.assets.sh
    should be configbump not traefik

--- codeready-workspaces-configbump/build/dockerfiles/rhel.Dockerfile.extract.assets.sh
+++ codeready-workspaces-configbump/build/dockerfiles/rhel.Dockerfile.extract.assets.sh
@@ -40,7 +40,7 @@ popd >/dev/null || exit 1
 if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
     curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
 fi
-./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix traefik "${WORKSPACE}/asset-configbump-${ARCH}.tar.gz"
+./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix configbump "${WORKSPACE}/asset-configbump-${ARCH}.tar.gz"
 
 
  #cleanup
