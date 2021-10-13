e5c1af9809681a270ef36bc2a19bf173e4c9a819
SDawley <88796121+SDawley@users.noreply.github.com>
2021-10-13 12:19:43 -0400

    Crw 2081 (#125)
    * ci: [sync] Update from traefik/traefik @ e0b44
    
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>
    
    * updating configbump rhel.Dockerfile.extract.assets.sh to upload tar to release (I think this is the correct file).
    
    * configdump get-sources should pull from GH not jenkins. Attempting to post theia to GH.
    
    * undo theia changes
    
    * had a traefik in copied code that should be configbump
    
    * Update rhel.Dockerfile.extract.assets.sh
    
    should be configbump not traefik
    
    Co-authored-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- codeready-workspaces-configbump/build/dockerfiles/rhel.Dockerfile.extract.assets.sh
+++ codeready-workspaces-configbump/build/dockerfiles/rhel.Dockerfile.extract.assets.sh
@@ -36,6 +36,13 @@ pushd ${TMPDIR} >/dev/null || exit 1
     tar cvzf "${WORKSPACE}/asset-configbump-${ARCH}.tar.gz" ./
 popd >/dev/null || exit 1
 
+# upload the binary to GH
+if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
+    curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
+fi
+./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix configbump "${WORKSPACE}/asset-configbump-${ARCH}.tar.gz"
+
+
  #cleanup
 ${PODMAN} rmi -f ${TMPIMG}
 rm -fr ${TMPDIR}
