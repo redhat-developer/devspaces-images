444330297ae47a3754098489fadf4c244d9460f2
SDawley <88796121+SDawley@users.noreply.github.com>
2021-10-18 12:12:36 -0400

    Crw 2081 configbump (#127)
    * feat: refactoring configbump to use sync.sh and rhel.binary.build.sh
    
    * fix: sync.sh needs CRW_VERSION not CSV_VERSION
    
    * fix: reference to SOURCE_DIR wasnt working in local build
    
    * fix: SOURCEDIR and TARGETDIR do not have underscores
    
    * fix: TARGETDIR does not have an underscore
    
    * ci: [sync] Update from che-incubator/configbump @ 0f3a
    
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>
    
    * remove unneeded \"
    
    * remove unneeded slash escape
    
    Co-authored-by: devstudio-release <nickboldt+devstudio-release@gmail.com>
    Co-authored-by: Nick Boldt <nboldt@redhat.com>

---
+++ codeready-workspaces-configbump/build/rhel.binary.build.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#
+# Copyright (c) 2021 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# build asset* files
+
+set -e
+
+# defaults
+CSV_VERSION=2.y.0 # csv 2.y.0
+UPLOAD_TO_GH=1
+
+MIDSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "crw-2-rhel-8")
+if [[ ${MIDSTM_BRANCH} != "crw-"*"-rhel-"* ]]; then MIDSTM_BRANCH="crw-2-rhel-8"; fi
+
+usage () {
+    echo "
+Usage:   $0 -v [CRW CSV_VERSION] [--noupload] [-b MIDSTM_BRANCH] [-ght GITHUB_TOKEN]
+Example: $0 -v 2.y.0 --noupload
+"
+    exit
+}
+
+if [[ $# -lt 1 ]]; then usage; fi
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+    '-v') CSV_VERSION="$2"; shift 1;;
+    '-b') MIDSTM_BRANCH="$2"; shift 1;;
+    '-ght') GITHUB_TOKEN="$2"; export GITHUB_TOKEN="${GITHUB_TOKEN}"; shift 1;;
+    '-n'|'--noupload') UPLOAD_TO_GH=0;;
+    '--help'|'-h') usage;;
+  esac
+  shift 1
+done
+
+#call rhel.Dockerfile.extract.asets
+ARCH="$(uname -m)"
+chmod +x ./build/dockerfiles/*.sh
+./build/dockerfiles/rhel.Dockerfile.extract.assets.sh
+
+if [[ ${UPLOAD_TO_GH} -eq 1 ]]; then
+  # upload the binary to GH
+  if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
+      curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
+  fi
+  ./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix configbump "${WORKSPACE}/asset-configbump-${ARCH}.tar.gz"
+fi
