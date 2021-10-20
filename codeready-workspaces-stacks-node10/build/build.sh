44207d823c28562ab5baa5b830262d3d298edd5e
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-10-20 12:48:33 -0700

    updating the call to updateAssets to use the --push-assets option

--- codeready-workspaces-stacks-node10/build/build.sh
+++ codeready-workspaces-stacks-node10/build/build.sh
@@ -64,6 +64,6 @@ tar -czf "target/codeready-workspaces-stacks-language-servers-dependencies-node1
 if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
     curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
 fi
-./uploadAssetsToGHRelease.sh -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix deprecated "target/codeready-workspaces-stacks-language-servers-dependencies-node10-$(uname -m).tar.gz"
+./uploadAssetsToGHRelease.sh --push-assets -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --prefix deprecated "target/codeready-workspaces-stacks-language-servers-dependencies-node10-$(uname -m).tar.gz"
 
 ${PODMAN} rmi -f ${NODEJS_IMAGE}
