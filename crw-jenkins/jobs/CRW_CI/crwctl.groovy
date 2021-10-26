af613749279631444a4d9f1806efe0b3e6ea3e8b
devstudio-release <nickboldt+devstudio-release@gmail.com>
2021-10-26 19:39:23 -0400

    chore: [jobs] Update from codeready-workspaces/crw-jenkins @ fa01
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- crw-jenkins/jobs/CRW_CI/crwctl.groovy
+++ crw-jenkins/jobs/CRW_CI/crwctl.groovy
@@ -90,9 +90,17 @@ if unset, version is CRW_VERSION-YYYYmmdd-commitSHA<br/>
 :: if suffix = GA, use server and operator tags from RHEC stage<br/>
 :: if suffix contains RC, use server and operator tags from Quay<br/>
 :: for all other suffixes, use server and operator tags = ''' + JOB_BRANCH + '''<br/>
-:: NOTE: yarn will fail for version = x.y.z.a but works with x.y.z-a''')
-                booleanParam("PUBLISH_ARTIFACTS_TO_GITHUB", false, "default false; check box to publish to GH releases")
-                booleanParam("PUBLISH_ARTIFACTS_TO_RCM", false, "default false; check box to upload sources + binaries to RCM for a GA release ONLY")
+:: NOTE: yarn will fail for version = x.y.z.a but works with x.y.z-a<br/>
+<br/>
+@since 2.13:<br/>
+* push all CI and RC bits to Github automatically (no more artifacts in Jenkins)<br/>
+* for GA suffix, push to RCM automatically (and copy to Github)
+''')
+
+                if (JOB_BRANCH.equals("2.12")) {
+                    booleanParam("PUBLISH_ARTIFACTS_TO_GITHUB", false, "default false; check box to publish to GH releases")
+                    booleanParam("PUBLISH_ARTIFACTS_TO_RCM", false, "default false; check box to upload sources + binaries to RCM for a GA release ONLY")
+                }
             }
 
             // Trigger builds remotely (e.g., from scripts), using Authentication Token = CI_BUILD
