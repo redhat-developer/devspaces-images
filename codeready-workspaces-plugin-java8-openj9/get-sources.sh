832752f670ddf3593fe3691d46fc41612013409f
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-10-27 16:08:27 -0700

    updating get-sources to use uploadAssetsToGH script to get assets. There's probably more jenkins stuff that can be taken out.

--- codeready-workspaces-plugin-java8-openj9/get-sources.sh
+++ codeready-workspaces-plugin-java8-openj9/get-sources.sh
@@ -15,6 +15,7 @@ while [[ "$#" -gt 0 ]]; do
 	'-p'|'--pull-assets') pullAssets=1; shift 0;;
 	'-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
 	'-t'|'--target') targetFlag="--target $2"; shift 1;;
+	'-v') CSV_VERSION="$2"; shift 1;;
 	*) JOB_BRANCH="$1"; shift 0;;
   esac
   shift 1
@@ -67,12 +68,7 @@ if [[ ! $jenkinsURL ]]; then
 	exit 1
 fi
 log "[INFO] Using Brew with ${targetFlag}" 
-theTarGzs="
-lastSuccessfulBuild/artifact/codeready-workspaces-deprecated/node10/target/codeready-workspaces-stacks-language-servers-dependencies-node10-s390x.tar.gz
-lastSuccessfulBuild/artifact/codeready-workspaces-deprecated/python/target/codeready-workspaces-stacks-language-servers-dependencies-python-s390x.tar.gz
-lastSuccessfulBuild/artifact/codeready-workspaces-deprecated/node10/target/codeready-workspaces-stacks-language-servers-dependencies-node10-ppc64le.tar.gz
-lastSuccessfulBuild/artifact/codeready-workspaces-deprecated/python/target/codeready-workspaces-stacks-language-servers-dependencies-python-ppc64le.tar.gz
-"
+
 lastSuccessfulURL="${jenkinsURL}/lastSuccessfulBuild/api/xml?xpath=/workflowRun/" # id
 # maven - install 3.6 from https://maven.apache.org/download.cgi
 MAVEN_VERSION="3.6.3"
@@ -156,31 +152,20 @@ function insertLabels () {
 	rm -f ${DOCKERFILE}.bak
 }
 
-function getFingerprints ()
-{
-	outputFile=$1
-	latestFingerprint="$(curl -L ${jenkinsURL}/lastSuccessfulBuild/fingerprints/ 2>&1 | grep ${outputFile} | sed -e "s#.\+/fingerprint/\([0-9a-f]\+\)/\".\+#\1#")"
-	currentFingerprint="$(cat sources | grep ${outputFile} | sed -e "s#\([0-9a-f]\+\) .\+#\1#")"
-}
-
-# get the public URL for the tarball(s)
-outputFiles=""
+theTarGzs="
+codeready-workspaces-stacks-language-servers-dependencies-node10-s390x.tar.gz
+codeready-workspaces-stacks-language-servers-dependencies-python-s390x.tar.gz
+codeready-workspaces-stacks-language-servers-dependencies-node10-ppc64le.tar.gz
+codeready-workspaces-stacks-language-servers-dependencies-python-ppc64le.tar.gz
+"
 
 #### override any existing tarballs with newer ones from Jenkins build
-for theTarGz in ${theTarGzs}; do
-	outputFile=${theTarGz##*/}
-	log "[INFO] Download ${jenkinsURL}/${theTarGz}:"
-	rm -f ${outputFile}
-	getFingerprints ${outputFile}
-	if [[ ! ${latestFingerprint} ]]; then 
-		echo "[WARNING] Cannot resolve artifact fingerprints for ${outputFile}"
-	fi
-	
-	if [[ "${latestFingerprint}" != "${currentFingerprint}" ]] || [[ ! -f ${outputFile} ]] || [[ ${pullAssets} -eq 1 ]]; then 
-		curl -L -o ${outputFile} ${jenkinsURL}/${theTarGz}
-		outputFiles="${outputFiles} ${outputFile}"
-	fi
-done
+if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
+    curl -sSLO "https://raw.githubusercontent.com/redhat-developer/codeready-workspaces/${JOB_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
+fi
+
+log "[INFO] Download Assets:"
+./uploadAssetsToGHRelease.sh --fetch-assets -v "${CSV_VERSION}" --prefix deprecated $theTarGzs
 
 # update Dockerfile to record version we expect for MAVEN_VERSION
 sed Dockerfile \
@@ -194,55 +179,53 @@ if [[ ! -f apache-maven-${MAVEN_VERSION}-bin.tar.gz ]] || [[ $(diff -U 0 --suppr
 	curl -sSL -O http://mirror.csclub.uwaterloo.ca/apache/maven/maven-3/${MAVEN_VERSION}/binaries/apache-maven-${MAVEN_VERSION}-bin.tar.gz
 	curl -sSL -O https://projectlombok.org/downloads/lombok-${LOMBOK_VERSION}.jar
 fi
-outputFiles="apache-maven-${MAVEN_VERSION}-bin.tar.gz lombok-${LOMBOK_VERSION}.jar ${outputFiles}"
+outputFiles="apache-maven-${MAVEN_VERSION}-bin.tar.gz lombok-${LOMBOK_VERSION}.jar ${theTarGzs}"
 
-if [[ ${outputFiles} ]]; then
-	log "[INFO] Upload new sources:${outputFiles}"
-	rhpkg new-sources ${outputFiles}
-	log "[INFO] Commit new sources from:${outputFiles}"
-	field=id; ID=$(curl -L -s -S ${lastSuccessfulURL}${field} | sed -e "s#<${field}>\(.\+\)</${field}>#\1#" -e "s#&lt;br/&gt; #\n#g" -e "s#\&lt;a.\+/a\&gt;##g")
-	if [[ $(echo $ID | grep -E "404 Not Found|ERROR 404|Application is not available") ]]; then 
-		echo $ID
-		echo "[ERROR] Problem loading ID from ${lastSuccessfulURL}${field} :: NOT FOUND!"
-		exit 1;
-	fi
-	COMMIT_MSG="Update from Jenkins :: Maven ${MAVEN_VERSION} + ${UPSTREAM_JOB_NAME} :: ${ID}
-::${outputFiles}"
-	parseCommitLog ${COMMIT_MSG}
-	insertLabels Dockerfile
-	if [[ $(git commit -s -m "ci: [get sources] ${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]] ;then 
-		log "[INFO] No new sources, so nothing to build."
-	elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-		log "[INFO] Push change:"
-		git pull; git push; git status -s || true
-	fi
-	if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-		echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
-		git status || true
-		tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+log "[INFO] Upload new sources:${theTarGzs}"
+rhpkg new-sources ${theTarGzs}
+log "[INFO] Commit new sources from:${theTarGzs}"
+field=id; ID=$(curl -L -s -S ${lastSuccessfulURL}${field} | sed -e "s#<${field}>\(.\+\)</${field}>#\1#" -e "s#&lt;br/&gt; #\n#g" -e "s#\&lt;a.\+/a\&gt;##g")
+if [[ $(echo $ID | grep -E "404 Not Found|ERROR 404|Application is not available") ]]; then 
+	echo $ID
+	echo "[ERROR] Problem loading ID from ${lastSuccessfulURL}${field} :: NOT FOUND!"
+	exit 1;
+fi
+COMMIT_MSG="Update from Jenkins :: Maven ${MAVEN_VERSION} + ${UPSTREAM_JOB_NAME} :: ${ID}
+::${theTarGzs}"
+parseCommitLog ${COMMIT_MSG}
+insertLabels Dockerfile
+if [[ $(git commit -s -m "ci: [get sources] ${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]] ;then 
+	log "[INFO] No new sources, so nothing to build."
+elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+	log "[INFO] Push change:"
+	git pull; git push; git status -s || true
+fi
+if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+	echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
+	git status || true
+	tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
 
 $ERRORS
 
 "; exit 1; fi
-	fi
+fi
+
+if [[ ${forceBuild} -eq 1 ]]; then
+	echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
+	git status || true
+	tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+
+$ERRORS
+
+"; exit 1; fi
 else
-	if [[ ${forceBuild} -eq 1 ]]; then
-		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
-		git status || true
-		tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
-
-$ERRORS
-
-"; exit 1; fi
-	else
-		log "[INFO] No new sources, so nothing to build."
-	fi
+	log "[INFO] No new sources, so nothing to build."
 fi
 
 # cleanup
