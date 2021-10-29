375db310168d2c90e967582ea44f6dfe35f66ba5
nickboldt <nboldt@redhat.com>
2021-10-29 17:36:22 -0300

    remove DWO and DWCO sources
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- codeready-workspaces-devworkspace/deploy/defaults.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-
-# Note that this file needs to work both in a shell script and in a makefile.
-# Refrain from anything else but defining variables. Don't even reference other variables.
-
-DEFAULT_DWCO_NAMESPACE=devworkspace-che
-DEFAULT_DWCO_IMG=quay.io/che-incubator/devworkspace-che-operator:ci
-DEFAULT_DWCO_PULL_POLICY=Always
