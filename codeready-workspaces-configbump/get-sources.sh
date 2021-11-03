6500ab4c47f767dac9663efc0ff205d8016bf0c8
Samantha Dawley <samantha.m.dawley@gmail.com>
2021-11-03 12:34:50 -0700

    Fixing logic of PULL_ASSETS

--- codeready-workspaces-configbump/get-sources.sh
+++ codeready-workspaces-configbump/get-sources.sh
@@ -80,32 +80,32 @@ if [[ ${PULL_ASSETS} -eq 1 ]]; then
 		log "[INFO] Push change:"
 		git pull; git push; git status -s || true
 	fi
-fi
 
-if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-	echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
-	git status || true
-	tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
-
-$ERRORS
-
-"; exit 1; fi
-fi
-
-if [[ ${forceBuild} -eq 1 ]]; then
-	echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
-	git status || true
-	tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+	if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+		echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
 
 $ERRORS
 
 "; exit 1; fi
+	fi
 else
-	log "[INFO] No new sources, so nothing to build."
+	if [[ ${forceBuild} -eq 1 ]]; then
+		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+
+$ERRORS
+
+	"; exit 1; fi
+	else
+		log "[INFO] No new sources, so nothing to build."
+	fi
 fi
--- codeready-workspaces-plugin-java8-openj9/get-sources.sh
+++ codeready-workspaces-plugin-java8-openj9/get-sources.sh
@@ -81,45 +81,43 @@ if [[ ${PULL_ASSETS} -eq 1 ]]; then
 	fi
 	outputFiles="apache-maven-${MAVEN_VERSION}-bin.tar.gz lombok-${LOMBOK_VERSION}.jar ${theTarGzs}"
 
-	log "[INFO] Upload new sources:${theTarGzs}"
-	rhpkg new-sources ${theTarGzs}
-	log "[INFO] Commit new sources from:${theTarGzs}"
-	COMMIT_MSG="Update from GitHub :: Maven ${MAVEN_VERSION} + ${ASSET_NAME} Assets ::${theTarGzs}"
+	log "[INFO] Upload new sources:${outputFiles}"
+	rhpkg new-sources ${outputFiles}
+	log "[INFO] Commit new sources from:${outputFiles}"
+	COMMIT_MSG="Update from GitHub :: Maven ${MAVEN_VERSION} + ${ASSET_NAME} Assets ::${outputFiles}"
 	if [[ $(git commit -s -m "ci: [get sources] ${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]] ;then 
 		log "[INFO] No new sources, so nothing to build."
 	elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
 		log "[INFO] Push change:"
 		git pull; git push; git status -s || true
 	fi
-fi
-
-if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-	echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
-	git status || true
-	tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+	if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+		echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
 
 $ERRORS
 
 "; exit 1; fi
-fi
-
-if [[ ${forceBuild} -eq 1 ]]; then
-	echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
-	git status || true
-	tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
-	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
-	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
-	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
-
-$ERRORS
-
-"; exit 1; fi
+	fi
 else
-	log "[INFO] No new sources, so nothing to build."
-fi
+	if [[ ${forceBuild} -eq 1 ]]; then
+		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${targetFlag} ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${targetFlag} ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+		taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+		ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
 
+$ERRORS
+
+"; exit 1; fi
+	else
+		log "[INFO] No new sources, so nothing to build."
+	fi
+fi
 # cleanup
 rm -fr Dockerfile.2
