8452de53f6d29dfbe9604be1dfd2342145a70cc8
devstudio-release <nickboldt+devstudio-release@gmail.com>
2021-11-15 07:37:55 -0500

    ci: [sync] Update from redhat-developer/codeready-workspaces @ c538
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- codeready-workspaces-devfileregistry/build/scripts/swap_images.sh
+++ codeready-workspaces-devfileregistry/build/scripts/swap_images.sh
@@ -10,18 +10,29 @@
 # script common to devfileregistry and pluginregistry; see also crw-operator/build/scripts/swap_images.sh
 
 SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd)
-YAML_ROOT="$1"
+
+FORCE_SWAP=0
+arch="$(uname -m)"
+
+while [[ "$#" -gt 0 ]]; do
+  case $1 in
+  '-f') FORCE_SWAP=1;; # force transformation even on an incompatible architecture for testing purposes
+  '-a') arch="$2"; shift 1;;
+  *) YAML_ROOT="$1";;
+  esac
+  shift 1
+done
 
 devfiles=$("$SCRIPT_DIR"/list_yaml.sh "$YAML_ROOT")
 
-# Note: optional -f flag will force this transformation even on an incompatible architecture,
-# so we can call this script from crw-operator/build/scripts/insert-related-images-to-csv.sh
 # shellcheck disable=SC2086
-if [[ "$(uname -m)" != "x86_64" ]] || [[ "$2" == "-f" ]]; then
+if [[ "$arch" != "x86_64" ]] || [[ $FORCE_SWAP -eq 1 ]]; then
+    echo -n "[INFO] swap openjdk for openj9 images on $arch"
+    if [[ $FORCE_SWAP -eq 1 ]]; then echo -n " (forced)"; fi
+    echo
     sed -E -i 's|plugin-java8-rhel8|plugin-java8-openj9-rhel8|g' $devfiles
     sed -E -i 's|plugin-java11-rhel8|plugin-java11-openj9-rhel8|g' $devfiles
-    sed -E -i 's|eap-xp2-openjdk11-openshift-rhel8:.*|eap-xp2-openj9-11-openshift-rhel8:2.0|g' $devfiles
     sed -E -i 's|eap-xp3-openjdk11-openshift-rhel8:.*|eap-xp3-openj9-11-openshift-rhel8:3.0|g' $devfiles
 else
-    echo "[INFO] nothing to do on $(uname -m); only swap openjdk for openj9 images on s390x and ppc64le arches"
+    echo "[INFO] nothing to do on $arch; only swap openjdk for openj9 images on s390x and ppc64le arches"
 fi
