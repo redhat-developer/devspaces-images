a76e8ac474293b01581fdf39aa93c2c1b950c727
nickboldt <nboldt@redhat.com>
2021-11-22 18:10:19 -0400

    remove the version suffix on both python-38:1 and httpd-24:1 to make brew stfu with its 'cant resolve base image' bullshit
    Signed-off-by: nickboldt <nboldt@redhat.com>

--- codeready-workspaces-pluginregistry/build/scripts/sync.sh
+++ codeready-workspaces-pluginregistry/build/scripts/sync.sh
@@ -106,8 +106,11 @@ sed "${TARGETDIR}/build/dockerfiles/Dockerfile" --regexp-extended \
     `# Strip registry from image references` \
     -e 's|FROM registry.access.redhat.com/|FROM |' \
     -e 's|FROM registry.redhat.io/|FROM |' \
-    `# CRW-2448 don't switch from ubi8 to rhel8 for OSBS` \
-    `# -e 's|ubi8/httpd-24:|rhel8/httpd-24:|g'` \
+    `# CRW-2448 switch from ubi8 to rhel8 for OSBS` \
+    -e 's|ubi8/httpd-24:([0-9]+)(-[0-9.]+)|rhel8/httpd-24:\1|g' \
+    -e 's|ubi8/httpd-24$|rhel8/httpd-24|g' \
+    `# trim off version so we get the latest from internal registry` \
+    -e 's|ubi8/python-38:([0-9]+)(-[0-9.]+)|ubi8/python-38:\1|g' \
     `# Set arg options: disable BOOTSTRAP; update CRW_BRANCH to correct value` \
     -e 's|ARG BOOTSTRAP=.*|ARG BOOTSTRAP=false|' \
     -e "s|ARG CRW_BRANCH=.*|ARG CRW_BRANCH=${CRW_BRANCH}|" \
