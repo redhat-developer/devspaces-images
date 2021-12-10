9fbdf46ddef12fe386b2f542dbc08853af6833d8
Mykhailo Kuznietsov <mkuznets@redhat.com>
2021-12-10 23:16:40 +0200

    fixup! fixup! CRW-1422 Add async-sidecar productized container

--- codeready-workspaces-async-storage-sidecar/build/scripts/sync.sh
+++ codeready-workspaces-async-storage-sidecar/build/scripts/sync.sh
@@ -64,6 +64,7 @@ sources
 yarn.lock
 /README.adoc
 /release.sh
+/common.sh
 /RELEASE.md
 .cico
 .circleci
@@ -76,12 +77,60 @@ rm -f /tmp/rsync-excludes
 # ensure shell scripts are executable
 find ${TARGETDIR}/ -name "*.sh" -exec chmod +x {} \;
 
+embed() { sed -e 's/^ *//; s/$/\\/'; }
+
+#### MULTILINE SED---
+build_image_multiline=$(embed << eof
+# build go content
+FROM rhel8/go-toolset:1.16.7-5 as builder
+ENV GOPATH=/tmp/go/
+
+USER root
+RUN adduser appuser
+
+COPY \$REMOTE_SOURCES \$REMOTE_SOURCES_DIR
+
+WORKDIR \$REMOTE_SOURCES_DIR/supercronic/app
+
+RUN source \$REMOTE_SOURCES_DIR/supercronic/cachito.env \
+  && make build
+
+WORKDIR /workspace-data-sync
+
+COPY . .
+
+# Make "vendoring" of github.com/gorilla/websocket dependency
+RUN mkdir -p vendor/github.com/gorilla/websocket \
+ && cp -rT \$REMOTE_SOURCES_DIR/websocket/app vendor/github.com/gorilla/websocket \
+ && go build -mod vendor -o ./dockerfiles/sidecar/scripts/watcher ./watcher/watcher.go
+eof
+)
+
 # transform build/*/Dockerfile -> Dockerfile
 sed ${SOURCEDIR}/dockerfiles/${UPSTM_IMAGE}/ubi.Dockerfile -r \
-  `# Replace ubi8 with rhel8 version` \
-  -e "s#FROM registry.redhat.io/#FROM #g" \
-  -e "s#FROM registry.access.redhat.com/#FROM #g" \
+  `#TODO: Remove supercronic downloading related code + simply use "useradd user"` \
+  `# Remove registry so build works in Brew` \
+  -e "s#FROM (registry.access.redhat.com|registry.redhat.io)/#FROM #g" \
+  `# Replace go-toolset ubi8 with rhel8 version` \
+  -e "s#ubi8/go-toolset#rhel8/go-toolset#g" \
+  `# Provide build artifacts from build image` \
+  -e '/FROM/a \
+COPY --from=builder /workspace-data-sync/dockerfiles/sidecar/scripts/watcher /watcher \
+COPY --from=builder $REMOTE_SOURCES_DIR/supercronic/app/supercronic /supercronic' \
+  `# Remove repos that don't work in Brew` \
+  -e '/content_sets_centos8/d' \
+  `# Provide scripts from builder image` \
+  -e 's#COPY cron/backup-cron-job#COPY --from=builder /workspace-data-sync/dockerfiles/sidecar/cron/backup-cron-job#g' \
+  -e 's#COPY scripts#COPY --from=builder /workspace-data-sync/dockerfiles/sidecar/s/scripts#g' \
+  `# Remove supercronic download & symlink` \
+  -e '/curl -fsSLO "$SUPERCRONIC_URL"/d' \
+  -e '/echo "${SUPERCRONIC_SHA1SUM}  ${SUPERCRONIC}"/d' \
+  `# Insert builder image in the beginning` \
+  -e "/# SPDX-License-Identifier: EPL-2.0/a \
+   $build_image_multiline
+" \
 > ${TARGETDIR}/Dockerfile
+
 cat << EOT >> ${TARGETDIR}/Dockerfile
 ENV SUMMARY="Red Hat CodeReady Workspaces ${MIDSTM_NAME} container" \\
     DESCRIPTION="Red Hat CodeReady Workspaces ${MIDSTM_NAME} container" \\
--- codeready-workspaces-async-storage-sidecar/common.sh
+++ codeready-workspaces-async-storage-sidecar/common.sh
@@ -8,7 +8,7 @@
 # SPDX-License-Identifier: EPL-2.0
 #
 
-IMAGE_AGENT=che-sidecar-workspace-data-sync
+IMAGE_SIDECAR=che-sidecar-workspace-data-sync
 IMAGE_STORAGE=che-workspace-data-sync-storage
 TAG=$1
 ORGANIZATION=$2
@@ -16,7 +16,7 @@ REPOSITORY=$3
 
 function compile() {
   echo "Compile file sync progress watcher binary from source code"
-  $(GOOS=linux go build -o ./dockerfiles/agent/scripts/watcher ./watcher/watcher.go)
+  $(GOOS=linux go build -o ./dockerfiles/sidecar/scripts/watcher ./watcher/watcher.go)
   if [ $? != 0 ]; then
     echo "Failed to compile code"
     exit 0
@@ -25,10 +25,10 @@ function compile() {
 }
 
 function dockerBuild() {
-  printf "Build docker image %s/%s/%s:%s \n" "$REPOSITORY" "$ORGANIZATION" "$IMAGE_AGENT" "$TAG";
-  docker build -t "$REPOSITORY/$ORGANIZATION/$IMAGE_AGENT:$TAG" ./dockerfiles/agent
+  printf "Build docker image %s/%s/%s:%s \n" "$REPOSITORY" "$ORGANIZATION" "$IMAGE_SIDECAR" "$TAG";
+  docker build -t "$REPOSITORY/$ORGANIZATION/$IMAGE_SIDECAR:$TAG" ./dockerfiles/sidecar
   if [ $? != 0 ]; then
-    printf "Failed build docker image %s/%s/%s:%s \n" "$REPOSITORY" "$ORGANIZATION" "$IMAGE_AGENT" "$TAG";
+    printf "Failed build docker image %s/%s/%s:%s \n" "$REPOSITORY" "$ORGANIZATION" "$IMAGE_SIDECAR" "$TAG";
     exit 0
   fi
 
