4a9d9963a1c3760173784c9db553816635131e03
devstudio-release <nickboldt+devstudio-release@gmail.com>
2022-04-13 19:39:35 -0400

    chore: [jobs] Update from codeready-workspaces/crw-jenkins @ 8af3
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- crw-jenkins/jobs/CRW_CI/Releng/create-branches.groovy
+++ crw-jenkins/jobs/CRW_CI/Releng/create-branches.groovy
@@ -1,6 +1,6 @@
 import groovy.json.JsonSlurper
 
-def curlCMD = "https://raw.github.com/redhat-developer/codeready-workspaces/crw-2-rhel-8/dependencies/job-config.json".toURL().text
+def curlCMD = "https://raw.github.com/redhat-developer/devspaces/devspaces-3-rhel-8/dependencies/job-config.json".toURL().text
 
 def jsonSlurper = new JsonSlurper();
 def config = jsonSlurper.parseText(curlCMD);
@@ -11,14 +11,14 @@ for (String JOB_BRANCH : JOB_BRANCHES) {
         // keep job disabled until we explicitly need it
         disabled(true)
 
-        MIDSTM_BRANCH="crw-2-rhel-8"
-        FUTURE_BRANCH="crw-"+JOB_BRANCH+"-rhel-8"
+        MIDSTM_BRANCH="devspaces-3-rhel-8"
+        FUTURE_BRANCH="devspaces-"+JOB_BRANCH+"-rhel-8"
 
         description('''
-This job is meant to be run after upstream deps are available to start the new crw-2.y-rhel-8 github branches for the upcoming release.
+This job is meant to be run after upstream deps are available to start the new devspaces-3.y-rhel-8 github branches for the upcoming release.
 <p>
-See <a href=https://github.com/redhat-developer/codeready-workspaces/blob/crw-2-rhel-8/product/tagRelease.sh>
-https://github.com/redhat-developer/codeready-workspaces/blob/crw-2-rhel-8/product/tagRelease.sh</a>
+See <a href=https://github.com/redhat-developer/devspaces/blob/devspaces-3-rhel-8/product/tagRelease.sh>
+https://github.com/redhat-developer/devspaces/blob/devspaces-3-rhel-8/product/tagRelease.sh</a>
 
 <p>To create branches of pkgs.devel repos, open a ticket like <a href=https://projects.engineering.redhat.com/browse/SPMM-4820>SPMM-4820</a>
 
@@ -39,7 +39,7 @@ https://github.com/redhat-developer/codeready-workspaces/blob/crw-2-rhel-8/produ
         }
 
         parameters{
-            stringParam("MIDSTM_BRANCH",MIDSTM_BRANCH,"redhat-developer/codeready-workspaces branch to use as source of the new branches")
+            stringParam("MIDSTM_BRANCH",MIDSTM_BRANCH,"redhat-developer/devspaces branch to use as source of the new branches")
             stringParam("FUTURE_BRANCH",FUTURE_BRANCH,"branch to create")
             booleanParam("CLEAN_ON_FAILURE", true, "If false, don't clean up workspace after the build so it can be used for debugging.")
         }
