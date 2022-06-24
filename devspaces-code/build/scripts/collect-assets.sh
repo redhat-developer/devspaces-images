83d50f2645a800ea249c654642ad85f95c62f9f3
Roman Nikitenko <rnikiten@redhat.com>
2022-06-24 16:19:34 +0300

    feat: Productize che-code as an OpenShift Dev Spaces editor
    Signed-off-by: Roman Nikitenko <rnikiten@redhat.com>

---
+++ devspaces-code/build/scripts/collect-assets.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+#
+# Copyright (c) 2022 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+
+LINUX_LIBC_IMAGE=linux-libc-amd64:latest
+MACHINE_EXEC_IMAGE=quay.io/eclipse/che-machine-exec:7.39.1
+
+SCRIPT_PATH="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
+BASE_DIR_PATH="${SCRIPT_PATH}/../.."
+DOCKERFILES_PATH="${BASE_DIR_PATH}/build/dockerfiles"
+
+#### linux-libc-content ####
+collect_linux_libc_content_assets() {
+    docker build -f "${DOCKERFILES_PATH}/linux-libc.Dockerfile" -t $LINUX_LIBC_IMAGE .
+
+    id="$(docker create $LINUX_LIBC_IMAGE)"
+    docker cp "$id":/checode-linux-libc - | gzip -9 > asset-linux-libc.tar.gz 
+    docker rm -v $id
+    docker rmi $(docker images $LINUX_LIBC_IMAGE -a -q)
+}
+
+#### machine-exec ####
+collect_machine_exec_assets() {
+    id="$(docker create $MACHINE_EXEC_IMAGE)"
+    docker cp "$id":/go/bin/che-machine-exec - | gzip -9 > asset-machine-exec.tar.gz 
+    docker rm -v $id
+    docker rmi $(docker images $MACHINE_EXEC_IMAGE -a -q)
+}
+
+### entrypoints ###
+collect_entrypoints_assets() {
+    (cd $SCRIPT_PATH && tar -cvf ${BASE_DIR_PATH}/asset-entrypoints.tar.gz entrypoint*.sh)
+}
+
+collect_machine_exec_assets
+collect_linux_libc_content_assets
+collect_entrypoints_assets
---
+++ devspaces-code/get-sources.sh
@@ -0,0 +1,96 @@
+#!/bin/bash -xe
+# script to trigger rhpkg after updating vendor folder
+#
+verbose=1
+scratchFlag=""
+doRhpkgContainerBuild=1
+forceBuild=0
+# NOTE: --pull-assets (-p) flag uses opposite behaviour to some other get-sources.sh scripts;
+# here we want to collect assets during sync-to-downsteam (using get-sources.sh -n -p)
+# so that rhpkg build is simply a brew wrapper (using get-sources.sh -f)
+PULL_ASSETS=0
+
+while [[ "$#" -gt 0 ]]; do
+	case $1 in
+		'-p'|'--pull-assets') PULL_ASSETS=1; shift 0;;
+		'-a'|'--publish-assets') exit 0; shift 0;;
+		'-d'|'--delete-assets') exit 0; shift 0;;
+		'-n'|'--nobuild') doRhpkgContainerBuild=0; shift 0;;
+		'-f'|'--force-build') forceBuild=1; shift 0;;
+		'-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
+		'-v') CSV_VERSION="$2"; shift 1;;
+	esac
+	shift 1
+done
+
+function log()
+{
+	if [[ ${verbose} -gt 0 ]]; then
+	echo "$1"
+	fi
+}
+
+if [[ ${PULL_ASSETS} -eq 1 ]]; then
+  ./build/scripts/collect-assets.sh 
+
+  if [[ ! -f "asset-linux-libc.tar.gz" ]]; then
+    log "[ERROR] 'asset-linux-libc.tar.gz' not found, so nothing to build."
+    exit 1;
+  fi
+
+  if [[ ! -f "asset-machine-exec.tar.gz" ]]; then
+    log "[ERROR] 'asset-machine-exec.tar.gz' not found, so nothing to build."
+    exit 1;
+  fi
+
+  if [[ ! -f "asset-entrypoints.tar.gz" ]]; then
+    log "[ERROR] 'asset-entrypoints.tar.gz' not found, so nothing to build."
+    exit 1;
+  fi
+
+  outputFiles="asset-linux-libc.tar.gz asset-machine-exec.tar.gz asset-entrypoints.tar.gz"
+fi
+
+if [[ $(git diff-index HEAD --) ]] || [[ ${PULL_ASSETS} -eq 1 ]]; then
+	git add sources Dockerfile .gitignore || true
+	log "[INFO] Upload new sources: ${outputFiles}"
+	# shellcheck disable=SC2086
+	rhpkg new-sources ${outputFiles}
+	log "[INFO] Commit new sources from: ${outputFiles}"
+	COMMIT_MSG="ci: ${outputFiles}"
+	if [[ $(git commit -s -m "${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]]; then
+		log "[INFO] No new sources, so nothing to build."
+	elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+		log "[INFO] Push change:"
+		git pull; git push; git status -s || true
+	fi
+	if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
+		echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 "$tmpfile"
+		# shellcheck disable=SC2002
+		taskID=$(cat "$tmpfile" | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs "$taskID" | tee 2>&1 "$tmpfile"
+		ERRORS="$(grep "image build failed" "$tmpfile")" && rm -f "$tmpfile"
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+
+$ERRORS
+
+"; exit 1; fi
+	fi
+else
+	if [[ ${forceBuild} -eq 1 ]]; then
+		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
+		git status || true
+		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 "$tmpfile"
+		# shellcheck disable=SC2002
+		taskID=$(cat "$tmpfile" | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs "$taskID" | tee 2>&1 "$tmpfile"
+		ERRORS="$(grep "image build failed" "$tmpfile")" && rm -f "$tmpfile"
+		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+
+$ERRORS
+
+"; exit 1; fi
+	else
+		log "[INFO] No new sources, so nothing to build."
+	fi
+fi
