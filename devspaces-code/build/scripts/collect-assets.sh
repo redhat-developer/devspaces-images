5fe3c546b3c24d76e84e122789d8f22c613d7988
Nick Boldt <nboldt@redhat.com>
2022-07-20 13:40:08 -0300

    chore: CRW-2744 implement publishing of multiarch assets to GH releases (until we can move to Cachito) (#296)
    Change-Id: I7f1cbb6e970ec7015a10003c977c149d8469f2e7
    Signed-off-by: Nick Boldt <nickboldt@gmail.com>

--- devspaces-code/build/scripts/collect-assets.sh
+++ devspaces-code/build/scripts/collect-assets.sh
@@ -13,7 +13,8 @@
 # commandline args
 while [[ "$#" -gt 0 ]]; do
   case $1 in
-    '-t') DS_VERSION="$2"; shift 1;; # get tag version (3.y), not csv version (3.y.z)
+	'-v') CSV_VERSION="$2"; DS_VERSION="${CSV_VERSION%.*}"; shift 1;;
+	'-ght') GITHUB_TOKEN="$2"; shift 1;;
   esac
   shift 1
 done
@@ -30,13 +31,14 @@ MACHINE_EXEC_IMAGE=quay.io/devspaces/machineexec-rhel8:$DS_VERSION
 SCRIPT_PATH="$(cd $(dirname "${BASH_SOURCE[0]}") && pwd)"
 BASE_DIR_PATH="${SCRIPT_PATH}/../.."
 DOCKERFILES_PATH="${BASE_DIR_PATH}/build/dockerfiles"
+mkdir -p target/brew-assets
 
 #### libc-content ####
 collect_libc_content_assets() {
     docker build -f "${DOCKERFILES_PATH}/libc-content-provider.Dockerfile" --build-arg GITHUB_TOKEN=$GITHUB_TOKEN -t $LIBC_CONTENT_IMAGE .
 
     id="$(docker create $LIBC_CONTENT_IMAGE)"
-    docker cp "$id":/checode-linux-libc - | gzip -9 > asset-libc-content-${ARCH}.tar.gz 
+    docker cp "$id":/checode-linux-libc - | gzip -9 > target/brew-assets/asset-libc-content-${ARCH}.tar.gz 
     docker rm -v $id
     docker rmi $(docker images $LIBC_CONTENT_IMAGE -a -q)
 }
@@ -46,10 +48,25 @@ collect_machine_exec_assets() {
     echo "Using ${MACHINE_EXEC_IMAGE} to prepare machine-exec asset"
 
     id="$(docker create $MACHINE_EXEC_IMAGE)"
-    docker cp "$id":/go/bin/che-machine-exec - | gzip -9 > asset-machine-exec.tar.gz 
+    docker cp "$id":/go/bin/che-machine-exec - | gzip -9 > target/brew-assets/asset-machine-exec-${ARCH}.tar.gz 
     docker rm -v $id
     docker rmi $(docker images $MACHINE_EXEC_IMAGE -a -q)
 }
 
 collect_machine_exec_assets
 collect_libc_content_assets
+
+if [[ ${UPLOAD_TO_GH} -eq 1 ]]; then
+  # upload the binary to GH
+  if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
+      curl -sSLO "https://raw.githubusercontent.com/redhat-developer/devspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
+  fi
+  # delete existing release & tag & assets -- this will remove everything for each push for each arch, so can't be done here
+  # ./uploadAssetsToGHRelease.sh --delete-assets -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --asset-name "${ASSET_NAME}"
+  # create a new release & tag w/ fresh assets
+  ./uploadAssetsToGHRelease.sh --publish-assets -v "${CSV_VERSION}" -b "${MIDSTM_BRANCH}" --asset-name "${ASSET_NAME}" target/brew-assets/asset-libc-content-${ARCH}.tar.gz  target/brew-assets/asset-machine-exec-${ARCH}.tar.gz 
+
+  # cleanup
+  podman rmi -f $TMP_IMG
+  rm -fr target/brew-assets
+fi
--- devspaces-code/get-sources.sh
+++ devspaces-code/get-sources.sh
@@ -1,25 +1,41 @@
 #!/bin/bash -xe
-# script to trigger rhpkg after updating vendor folder
+# script to trigger rhpkg after fetching assets from GH
 #
 verbose=1
 scratchFlag=""
 doRhpkgContainerBuild=1
 forceBuild=0
-# NOTE: --pull-assets (-p) flag uses opposite behaviour to some other get-sources.sh scripts;
 # here we want to collect assets during sync-to-downsteam (using get-sources.sh -n -p)
 # so that rhpkg build is simply a brew wrapper (using get-sources.sh -f)
 PULL_ASSETS=0
-ARCH="$(uname -m)"
+PUBLISH_ASSETS=0
+DELETE_ASSETS=0
+ASSET_NAME="code"
+GHT_STRING=""
+
+# compute project name from current dir
+SCRIPT_DIR=$(cd "$(dirname "$0")" || exit; pwd); 
+projectName=${SCRIPT_DIR##*/}; projectName=${projectName/devspaces-/}; 
+if [[ $projectName == "sources" ]]; then # compute a new string
+	projectName=$(git config --local remote.origin.url|sed -n 's#.*/\([^.]*\)#\1#p' | sed -e "s#devspaces-##" -e "s#\.git##")
+fi
+# echo $projectName
+
+# compute CSV_VERSION from MIDSTM_BRANCH
+MIDSTM_BRANCH=$(git rev-parse --abbrev-ref HEAD 2>/dev/null || echo "devspaces-3-rhel-8")
+if [[ ${MIDSTM_BRANCH} != "devspaces-"*"-rhel-"* ]]; then MIDSTM_BRANCH="devspaces-3-rhel-8"; fi
+CSV_VERSION=$(curl -sSLo- "https://raw.githubusercontent.com/redhat-developer/devspaces-images/${MIDSTM_BRANCH}/devspaces-operator-bundle/manifests/devspaces.csv.yaml" | yq -r .spec.version)
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
+		'-d'|'--delete-assets') DELETE_ASSETS=1; shift 0;;
+		'-a'|'--publish-assets') PUBLISH_ASSETS=1; shift 0;;
 		'-p'|'--pull-assets') PULL_ASSETS=1; shift 0;;
-		'-a'|'--publish-assets') exit 0; shift 0;;
-		'-d'|'--delete-assets') exit 0; shift 0;;
 		'-n'|'--nobuild') doRhpkgContainerBuild=0; shift 0;;
 		'-f'|'--force-build') forceBuild=1; shift 0;;
 		'-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
-		'-v') CSV_VERSION="$2"; DS_VERSION="${CSV_VERSION%.*}"; shift 1;;
+		'-v') CSV_VERSION="$2"; shift 1;;
+		'-ght') GITHUB_TOKEN="$2"; GHT_STRING="-ght GITHUB_TOKEN"; shift 1;;
 	esac
 	shift 1
 done
@@ -31,29 +47,37 @@ function log()
 	fi
 }
 
-if [[ ${PULL_ASSETS} -eq 1 ]]; then
-  ./build/scripts/collect-assets.sh -t $DS_VERSION 
-
-  if [[ ! -f "asset-libc-content-${ARCH}.tar.gz" ]]; then
-    log "[ERROR] 'asset-libc-content-${ARCH}.tar.gz' not found, so cannot proceed with build."
-    exit 1;
-  fi
-
-  if [[ ! -f "asset-machine-exec.tar.gz" ]]; then
-    log "[ERROR] 'asset-machine-exec.tar.gz' not found, so cannot proceed with build."
-    exit 1;
-  fi
-
-  outputFiles="asset-libc-content-${ARCH}.tar.gz asset-machine-exec.tar.gz"
+if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
+    curl -sSLO "https://raw.githubusercontent.com/redhat-developer/devspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
 fi
 
-if [[ $(git diff-index HEAD --) ]] || [[ ${PULL_ASSETS} -eq 1 ]]; then
-	git add sources Dockerfile .gitignore || true
-	log "[INFO] Upload new sources: ${outputFiles}"
+if [[ ${DELETE_ASSETS} -eq 1 ]]; then
+	log "[INFO] Delete ${CSV_VERSION} ${ASSET_NAME} assets and GH release:"
+	./uploadAssetsToGHRelease.sh --delete-assets -v "${CSV_VERSION}" -n ${ASSET_NAME}
+	exit 0;
+fi
+
+if [[ ${PUBLISH_ASSETS} -eq 1 ]]; then
+	log "[INFO] Build ${CSV_VERSION} ${ASSET_NAME} assets and publish to GH release:"
+	./build/scripts/collect-assets.sh -v ${CSV_VERSION} -n ${ASSET_NAME} ${GHT_STRING}
+	exit 0
+fi
+
+if [[ ${PULL_ASSETS} -eq 1 ]]; then
+	#### override any existing tarballs with newer ones from asset build
+	log "[INFO] Download ${CSV_VERSION} ${ASSET_NAME} assets:"
+	REPO_PATH=""
+	if [[ -d ${WORKSPACE}/sources/ ]]; then REPO_PATH="--repo-path ${WORKSPACE}/sources"; fi
+	./uploadAssetsToGHRelease.sh --pull-assets -v "${CSV_VERSION}" -n ${ASSET_NAME} ${REPO_PATH} --target "${TARGETDIR}"
+
+	#get names of the downloaded tarballs
+	theTarGzs="$(ls *.tar.gz)"
+
+	log "[INFO] Upload new sources: ${theTarGzs}"
 	# shellcheck disable=SC2086
-	rhpkg new-sources ${outputFiles}
-	log "[INFO] Commit new sources from: ${outputFiles}"
-	COMMIT_MSG="ci: ${outputFiles}"
+	rhpkg new-sources ${theTarGzs}
+	log "[INFO] Commit new sources from: ${theTarGzs}"
+	COMMIT_MSG="ci: GH ${ASSET_NAME} assets :: ${theTarGzs}"
 	if [[ $(git commit -s -m "${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]]; then
 		log "[INFO] No new sources, so nothing to build."
 	elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
@@ -74,6 +98,8 @@ $ERRORS
 "; exit 1; fi
 	fi
 else
+	# cleanup
+	rm -f uploadAssetsToGHRelease.sh
 	if [[ ${forceBuild} -eq 1 ]]; then
 		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
 		git status || true
