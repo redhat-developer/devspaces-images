b7e056a0a94195363d2f322ad814d2087560af5f
devstudio-release <nickboldt+devstudio-release@gmail.com>
2022-11-28 15:59:33 -0500

    ci: [sync] Update from che-incubator/che-code @ e1e18f
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-code/code/src/vs/platform/actionWidget/browser/actionList.ts
+++ devspaces-code/code/src/vs/platform/actionWidget/browser/actionList.ts
@@ -169,11 +169,15 @@ export class ActionList<T extends IActionItem> extends Disposable {
 		this.domNode = document.createElement('div');
 		this.domNode.classList.add('actionList');
 		const virtualDelegate: IListVirtualDelegate<IListMenuItem<IActionItem>> = {
-			getHeight: element => element.kind === 'header' ? this._headerLineHeight : this._actionLineHeight,
+			getHeight: element => element.kind === ActionListItemKind.Header ? this._headerLineHeight : this._actionLineHeight,
 			getTemplateId: element => element.kind
 		};
-		this._list = new List(user, this.domNode, virtualDelegate, [new ActionItemRenderer<IListMenuItem<IActionItem>>(resolver, this._keybindingService), new HeaderRenderer()], {
-			keyboardSupport: true,
+
+		this._list = this._register(new List(user, this.domNode, virtualDelegate, [
+			new ActionItemRenderer<IListMenuItem<IActionItem>>(resolver, this._keybindingService),
+			new HeaderRenderer()
+		], {
+			keyboardSupport: false,
 			accessibilityProvider: {
 				getAriaLabel: element => {
 					if (element.kind === 'action') {
@@ -189,7 +193,7 @@ export class ActionList<T extends IActionItem> extends Disposable {
 				getRole: () => 'option',
 				getWidgetRole: () => user
 			},
-		});
+		}));
 
 		this._register(this._list.onMouseClick(e => this.onListClick(e)));
 		this._register(this._list.onMouseOver(e => this.onListHover(e)));
