f415c693feeb10d605c75e3ddcdd82dcc51cc49e
Nick Boldt <nboldt@redhat.com>
2022-11-28 20:04:38 -0400

    apply changes in Dockerfile via sync.sh script (CRW-3099)
    Change-Id: Ic39ec635f1a01ac67f4d7a81bd63afe031eafc52
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- devspaces-server/build/scripts/sync.sh
+++ devspaces-server/build/scripts/sync.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 #
-# Copyright (c) 2021 Red Hat, Inc.
+# Copyright (c) 2021-2022 Red Hat, Inc.
 # This program and the accompanying materials are made
 # available under the terms of the Eclipse Public License 2.0
 # which is available at https://www.eclipse.org/legal/epl-2.0/
@@ -19,6 +19,13 @@ SCRIPTS_DIR=$(cd "$(dirname "$0")"; pwd)
 # defaults
 CSV_VERSION=2.y.0 # csv 2.y.0
 DS_VERSION=${CSV_VERSION%.*} # tag 2.y
+
+# TODO CRW-3099 compute these on the fly, rather than hardcoding
+
+# https://orch.psi.redhat.com/pnc-web/#/projects/1274/build-configs/8937/builds/AVMHG3CJC3YAA
+pnc_build_id="20221128-2146"
+# https://orch.psi.redhat.com/pnc-web/#/artifacts/9377523 == org.eclipse.che:assembly-main:tar.gz:7.yy.0.redhat-0000z
+pnc_artifact_id="9377523"
 
 usage () {
     echo "
@@ -56,9 +63,9 @@ build/scripts/sync.sh
 /content_sets.*
 /cvp.yml
 /cvp-owners.yml
+/fetch-artifacts-pnc.yaml
 get-source*.sh
 tests/basic-test.yaml
-sources
 .ci
 .dependabot
 .repositories-update-contributing.sh
@@ -85,6 +92,9 @@ rsync -azrlt --checksum ${SOURCEDIR}/dockerfiles/che/entrypoint.sh ${TARGETDIR}
 # ensure shell scripts are executable
 find ${TARGETDIR}/ -name "*.sh" -exec chmod +x {} \;
 
+# CRW-3099 set builds.build_id and builds.build_id.artifacts.id in fetch-artifacts-pnc.yaml
+sed -i ${TARGETDIR}/fetch-artifacts-pnc.yaml -r -e "s@(- build_id: ).+@\1'${pnc_build_id}'@" -e "s@(- id: ).+@\1'${pnc_artifact_id}'@"
+
 # NOTE: upstream Dockerfile is in non-standard path (not build/dockerfiles/Dockerfile) because project has multiple container builds
 sed ${SOURCEDIR}/dockerfiles/che/Dockerfile -r \
     `# Strip registry from image references` \
@@ -92,7 +102,7 @@ sed ${SOURCEDIR}/dockerfiles/che/Dockerfile -r \
     -e 's|FROM registry.redhat.io/|FROM |' \
     -e 's@/home/user/eclipse-che@/home/user/devspaces@g' \
 	`# insert logic to unpack asset-*.tgz` \
-    -e 's@ADD eclipse-che .+@# NOTE: To build in Brew, use get-source*.sh to build new server.tgz\nCOPY asset-server.tgz /tmp/asset-server.tgz\nRUN tar xzf /tmp/asset-server.tgz --strip-components=1 -C /home/user/devspaces; rm -f /tmp/asset-server.tgz\n@g' \
+    -e 's@ADD eclipse-che .+@# see fetch-artifacts-pnc.yaml\nCOPY artifacts/assembly-main.tar.gz /tmp/assembly-main.tar.gz\nRUN tar xzf /tmp/assembly-main.tar.gz --strip-components=1 -C /home/user/devspaces; rm -f /tmp/assembly-main.tar.gz\n@g' \
     -e 's@chmod g\\+w /home/user/cacerts@chmod 777 /home/user/cacerts@g' \
 > ${TARGETDIR}/Dockerfile
 cat << EOT >> ${TARGETDIR}/Dockerfile
@@ -108,6 +118,8 @@ LABEL summary="\$SUMMARY" \\
       com.redhat.component="\$PRODNAME-\$COMPNAME-container" \\
       name="\$PRODNAME/\$COMPNAME" \\
       version="${DS_VERSION}" \\
+      pnc_artifact_id="${pnc_artifact_id}" \\
+      pnc_build_id="${pnc_build_id}" \\
       license="EPLv2" \\
       maintainer="Nick Boldt <nboldt@redhat.com>" \\
       io.openshift.expose-services="" \\
