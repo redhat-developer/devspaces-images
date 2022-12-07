6f7e1a679adb315c6a85e8d8c271c550a70ffa70
Nick Boldt <nboldt@redhat.com>
2022-12-06 23:47:24 -0400

    CRW-3487 remove old local builders for php-ls and xdebug; move sources for xdebug into cachito; remove get-sources.sh (make it the simple generic on) and remove sources file
    Change-Id: Ia8844e2f4b2669faad29357744053bd927c0bfdc
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

---
+++ devspaces-udi/build/php_xdebug.Dockerfile
@@ -0,0 +1,55 @@
+# Copyright (c) 2022 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# this dockerfile is not part of the UDI image build, but can be used to build only the php+xdebug part of the UDI image locally, 
+# to facilitate testing before using Cachito sources in OSBS
+
+FROM ubi8-minimal:latest
+
+USER root
+ENV \
+    HOME=/home/user \
+    PHP_VERSION="7.4" \
+    XDEBUG_VERSION="3.1.6"
+
+RUN \
+    microdnf install -y dnf bash tar gzip unzip bzip2 which shadow-utils findutils wget curl sudo git git-lfs procps-ng 
+RUN \
+    dnf -y -q module enable php:$PHP_VERSION && \
+    dnf -y -q install php php-fpm php-opcache php-devel php-pear php-gd php-mysqli php-zlib php-curl ca-certificates make
+
+# COPY $REMOTE_SOURCES $REMOTE_SOURCES_DIR
+ENV REMOTE_SOURCES_DIR=/tmp/
+RUN cd $REMOTE_SOURCES_DIR; curl -sSLkO https://github.com/xdebug/xdebug/archive/refs/tags/${XDEBUG_VERSION}.tar.gz && \
+    tar xvzf ${XDEBUG_VERSION}.tar.gz && mv xdebug-${XDEBUG_VERSION} xdebug
+
+RUN \
+    # compile xdebug
+    cd $REMOTE_SOURCES_DIR/xdebug && \
+    # ls -la . && \
+    # note that we need the same value from phpize and phpize | grep "Extension Api"
+    # Zend Extension Build => API320180731,NTS
+    # Zend Extension Api No:   320180731
+    php -i | grep "Zend Extension Build" && phpize | grep "Extension Api" && \
+    ./configure --enable-xdebug && make && make install && \
+    # do we need all these settings? or just the zend_extension?
+    echo -e "zend_extension=$(find /usr/lib64/php/modules -name xdebug.so)\n\
+xdebug.client_port = 9000\n\
+xdebug.mode = debug\n\
+xdebug.start_with_request = yes\n\
+xdebug.remote_log=/tmp/xdebug.log" > /etc/php.ini && \
+    # set up httpd
+    sed -i 's/opt\/app-root\/src/projects/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/#DocumentRoot/DocumentRoot/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/CustomLog \"|\/usr\/bin\/cat\"/CustomLog \"\/var\/log\/httpd\/access_log\"/' /etc/httpd/conf/httpd.conf && \
+    sed -i 's/ErrorLog \"|\/usr\/bin\/cat\"/ErrorLog \"\/var\/log\/httpd\/error_log\"/' /etc/httpd/conf/httpd.conf && \
+    chmod -R 777 /var/run/httpd /var/log/httpd/ /etc/pki/ /etc/httpd/logs/ && \
+    # verify xdebug works
+    php --ini; echo "<?php xdebug_info() ?>" | php; php -v
--- devspaces-udi/get-sources.sh
+++ devspaces-udi/get-sources.sh
@@ -1,107 +1,33 @@
 #!/bin/bash -xe
-# script to build/fetch sources, then trigger rhpkg 
-TARGETDIR=$(cd "$(dirname "$0")"; pwd)
-verbose=1
+# script to get dependencies and trigger rhpkg build
 scratchFlag=""
 doRhpkgContainerBuild=1
 forceBuild=0
-PULL_ASSETS=0
-DELETE_ASSETS=0
-PUBLISH_ASSETS=0
-ASSET_NAME="udi"
 
 while [[ "$#" -gt 0 ]]; do
 	case $1 in
-	'-n'|'--nobuild') doRhpkgContainerBuild=0; shift 0;;
-	'-f'|'--force-build') forceBuild=1; shift 0;;
-	'-p'|'--pull-assets') PULL_ASSETS=1; shift 0;;
-	'-d'|'--delete-assets') DELETE_ASSETS=1; shift 0;;
-	'-a'|'--publish-assets') PUBLISH_ASSETS=1; shift 0;;
-	'-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
-	'-v') CSV_VERSION="$2"; shift 1;;
+		'-p'|'--pull-assets') exit 0; shift 0;;
+		'-a'|'--publish-assets') exit 0; shift 0;;
+		'-d'|'--delete-assets') exit 0; shift 0;;
+		'-n'|'--nobuild') doRhpkgContainerBuild=0; shift 0;;
+		'-f'|'--force-build') forceBuild=1; shift 0;;
+		'-s'|'--scratch') scratchFlag="--scratch"; shift 0;;
+		'-v') CSV_VERSION="$2"; shift 1;;
 	esac
 	shift 1
 done
 
-function log()
-{
-	if [[ ${verbose} -gt 0 ]]; then
-		echo "$1"
-	fi
-}
-
-if [[ ! -x ./uploadAssetsToGHRelease.sh ]]; then 
-    curl -sSLO "https://raw.githubusercontent.com/redhat-developer/devspaces/${MIDSTM_BRANCH}/product/uploadAssetsToGHRelease.sh" && chmod +x uploadAssetsToGHRelease.sh
-fi
-
-if [[ ${DELETE_ASSETS} -eq 1 ]]; then
-	log "[INFO] Delete ${CSV_VERSION} ${ASSET_NAME} assets and GH release:"
-	./uploadAssetsToGHRelease.sh --delete-assets -v "${CSV_VERSION}" -n ${ASSET_NAME} || true
-	exit 0;
-fi
-
-if [[ ${PUBLISH_ASSETS} -eq 1 ]]; then
-	log "[INFO] Build ${CSV_VERSION} ${ASSET_NAME} assets and publish to GH release:"
-	./build/build.sh -v ${CSV_VERSION} -n ${ASSET_NAME}
-	exit 0
-fi
-
-if [[ $(diff -U 0 --suppress-common-lines -b Dockerfile.2 Dockerfile) ]] || [[ ${PULL_ASSETS} -eq 1 ]]; then
-    rm -fr asset-*
-	mv -f Dockerfile.2 Dockerfile
-
-    log "[INFO] Download ${CSV_VERSION} ${ASSET_NAME} assets:"
-	REPO_PATH=""
-	if [[ -d ${WORKSPACE}/sources/ ]]; then REPO_PATH="--repo-path ${WORKSPACE}/sources"; fi
-
-	# pull asset-*.tar.gz files
-	./uploadAssetsToGHRelease.sh --pull-assets -v "${CSV_VERSION}" -n ${ASSET_NAME} ${REPO_PATH} --target "${TARGETDIR}"
-
-    outputFiles="$(ls asset-*.tar.gz)"
-
-	# cleanup
-	rm -f Dockerfile.2 uploadAssetsToGHRelease.sh
-
-	log "[INFO] Upload new sources: ${outputFiles}"
-	rhpkg new-sources ${outputFiles}
-	log "[INFO] Commit new sources from: ${outputFiles}"
-	COMMIT_MSG="ci: GH ${ASSET_NAME} assets :: ${outputFiles}"
-
-	if [[ $(git commit -s -m "${COMMIT_MSG}" sources Dockerfile .gitignore) == *"nothing to commit, working tree clean"* ]]; then
-		log "[INFO] No new sources, so nothing to build."
-	elif [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-		log "[INFO] Push change:"
-		git pull; git push; git status -s || true
-	fi
-	if [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
-		echo "[INFO] #1 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
-		git status || true
-		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 "$tmpfile"
-		# shellcheck disable=SC2002
-		taskID=$(cat "$tmpfile" | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs "$taskID" | tee 2>&1 "$tmpfile"
-		ERRORS="$(grep "image build failed" "$tmpfile")" && rm -f "$tmpfile"
-		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
+if [[ ${doRhpkgContainerBuild} -eq 1 ]] || [[ ${forceBuild} -eq 1 ]]; then
+	echo "[INFO] #0 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
+	git status || true
+	tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 $tmpfile
+	taskID=$(cat $tmpfile | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 $tmpfile
+	ERRORS="$(grep "image build failed" $tmpfile)" && rm -f $tmpfile
+	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
 
 $ERRORS
 
 "; exit 1; fi
-	fi
 else
-	# cleanup
-	rm -f Dockerfile.2 uploadAssetsToGHRelease.sh
-	if [[ ${forceBuild} -eq 1 ]]; then
-		echo "[INFO] #2 Trigger container-build in current branch: rhpkg container-build ${scratchFlag}"
-		git status || true
-		tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} --nowait | tee 2>&1 "$tmpfile"
-		# shellcheck disable=SC2002
-		taskID=$(cat "$tmpfile" | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs "$taskID" | tee 2>&1 "$tmpfile"
-		ERRORS="$(grep "image build failed" "$tmpfile")" && rm -f "$tmpfile"
-		if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
-
-$ERRORS
-
-"; exit 1; fi
-	else
-		log "[INFO] No new sources, so nothing to build."
-	fi
+	echo "[INFO] No build triggered, use -f or --force-build to build in Brew."
 fi
