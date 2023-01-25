2e927abc180682c045e0eedecaeb8acea39d98d1
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-01-25 15:10:35 -0500

    chore: [jobs] Update from codeready-workspaces/crw-jenkins @ 61aa
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- crw-jenkins/jobs/DS_CI/Releng/jiralint.groovy
+++ crw-jenkins/jobs/DS_CI/Releng/jiralint.groovy
@@ -1,6 +1,12 @@
 pipelineJob("${FOLDER_PATH}/${ITEM_NAME}"){
     disabled(false)
-    description('''Generate daily lint reports of jira issues''')
+    description('''<p>Generate daily lint reports of <a href=https://issues.redhat.com/browse/CRW>CRW jira</a> hygiene</p>
+<ul>
+<li>a <a href=https://github.com/redhat-developer/devspaces-jiralint/blob/devspaces-3-rhel-8/reports-weekly.json>weekly report</a> Sundays @ 22h00 and </li>
+<li><a href=https://github.com/redhat-developer/devspaces-jiralint/blob/devspaces-3-rhel-8/reports-daily.json>daily reports</a> Monday to Thursday @ 22h00</li>
+</ul><br/>
+To override the normal report type to today, choose a different type. <br/>
+Use <b>dry run</b> flag and your own email address to control output before sending emails to component owners.''')
 
     properties {
         ownership {
@@ -33,10 +39,11 @@ pipelineJob("${FOLDER_PATH}/${ITEM_NAME}"){
     }
 
     parameters{
-        booleanParam("DRY_RUN", false, "check box for dry run only (no issues will be created or updated)")
-        booleanParam("DEBUG", false, "check box for more console output")
-        stringParam("LIMIT", "200", "default: 200 results returned from JIRA queries")
-        stringParam("TO_EMAIL", "", "to override sending mail to component owners, enter your email address here")
+        booleanParam("DRY_RUN", false, "Check box for dry run only - no emails will be sent")
+        booleanParam("DEBUG", false, "Check box for more console output")
+        stringParam("LIMIT", "200", "Default: 200 results returned from JIRA queries")
+        stringParam("TO_EMAIL", "", "To override sending mail to component owners, enter your email address here")
+        choiceParam("WHICH_REPORT", ['default','daily','weekly'], "Choose daily or weekly to create that report")
     }
 
     definition {
