229937fed05d9b6f67d8e1930859057759aef635
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-01-31 10:31:52 -0500

    ci: [sync] Update from eclipse-che/che-dashboard @ fa6e1
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/.ci/oci-dashboard-puppeteer.sh
+++ devspaces-dashboard/.ci/oci-dashboard-puppeteer.sh
@@ -19,7 +19,7 @@ set -u
 # uncomment to print each command before executing it
 set -x
 
-export CI_CHE_DASHBOARD_IMAGE="quay.io/eclipse/che-dashboard:next"
+export CI_CHE_DASHBOARD_IMAGE=${CI_CHE_DASHBOARD_IMAGE:-quay.io/eclipse/che-dashboard:next}
 export CHE_REPO_BRANCH="main"
 export CHE_NAMESPACE="${CHE_NAMESPACE:-eclipse-che}"
 export DEVWORKSPACE_HAPPY_PATH="https://raw.githubusercontent.com/eclipse/che/${CHE_REPO_BRANCH}/tests/devworkspace-happy-path"
