00dfe4a1e376636f15af8d5086f3a9b1585623a6
Nick Boldt <nboldt@redhat.com>
2023-02-17 17:22:12 -0400

    chore: CRW-3196 add python config for yq in devfile reg container
    Change-Id: I9a59440f5420f5e02151718ec5aa4ad1d8bce2b4
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

---
+++ devspaces-devfileregistry/build/python/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='Python dependencies',
+      version='1.0',
+      description='Python dependencies',
+      author='Mykhailo Kuznietsov',
+      author_email='mkuznets@redhat.com',
+     )
