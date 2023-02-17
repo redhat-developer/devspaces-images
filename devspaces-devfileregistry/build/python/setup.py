249ada6844d7913f5a6210d75436779a4861add7
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-02-17 17:20:05 -0500

    ci: [sync] Update from redhat-developer/devspaces @ 5f4e
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-devfileregistry/build/python/setup.py
+++
@@ -1,8 +0,0 @@
-from setuptools import setup
-
-setup(name='Python dependencies',
-      version='1.0',
-      description='Python dependencies',
-      author='Mykhailo Kuznietsov',
-      author_email='mkuznets@redhat.com',
-     )
