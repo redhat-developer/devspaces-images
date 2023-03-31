277862b89bbe7b5f96dd07927eb496a36d47295f
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-03-31 10:11:15 -0400

    ci: [sync] Update from eclipse-che/che-dashboard @ a46b0b
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/packages/dashboard-frontend/src/services/helpers/__tests__/location.spec.ts
+++ devspaces-dashboard/packages/dashboard-frontend/src/services/helpers/__tests__/location.spec.ts
@@ -24,9 +24,9 @@ describe('location/sanitizeLocation', () => {
     expect(newLocation.pathname).toEqual(pathname);
   });
 
-  it('should return sanitized value of location.search', () => {
+  it('should return sanitized value of location.search if it is without encoding)', () => {
     const search =
-      '?url=https%3A%2F%2Fgithub.com%2Ftest-samples&state=9284564475&session=98765&session_state=45645654567&code=9844646765&storageType=persistent';
+      '?url=https://github.com/test-samples&state=9284564475&session=98765&session_state=45645654567&code=9844646765&storageType=persistent';
     const pathname = '/f';
 
     const newLocation = sanitizeLocation({ search, pathname } as Location);
@@ -34,6 +34,20 @@ describe('location/sanitizeLocation', () => {
     expect(newLocation.search).not.toEqual(search);
     expect(newLocation.search).toEqual(
       '?url=https%3A%2F%2Fgithub.com%2Ftest-samples&storageType=persistent',
+    );
+    expect(newLocation.pathname).toEqual(pathname);
+  });
+
+  it('should return sanitized value of location.search if it is encoded', () => {
+    const search =
+      '?url=https%3A%2F%2Fgithub.com%2Ftest-samples%26state%3D9284564475%26session%3D98765%26session_state%3D45645654567%26code%3D9844646765%26storageType%3Dpersistent';
+    const pathname = '/f';
+
+    const newLocation = sanitizeLocation({ search, pathname } as Location);
+
+    expect(newLocation.search).not.toEqual(search);
+    expect(newLocation.search).toEqual(
+      '?url=https%3A%2F%2Fgithub.com%2Ftest-samples%26storageType%3Dpersistent',
     );
     expect(newLocation.pathname).toEqual(pathname);
   });
