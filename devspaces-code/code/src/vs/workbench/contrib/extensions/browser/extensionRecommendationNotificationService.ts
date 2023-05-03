6cd854ee25e85a82bd871ff2d01b5fe3c60b9ad2
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-05-03 12:58:36 -0400

    ci: [sync] Update from che-incubator/che-code @ 0b592e
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-code/code/src/vs/workbench/contrib/extensions/browser/extensionRecommendationNotificationService.ts
+++ devspaces-code/code/src/vs/workbench/contrib/extensions/browser/extensionRecommendationNotificationService.ts
@@ -200,7 +200,7 @@ export class ExtensionRecommendationNotificationService implements IExtensionRec
 		});
 
 		if (result === RecommendationsNotificationResult.Accepted) {
-			this.storageService.store(donotShowWorkspaceRecommendationsStorageKey, true, StorageScope.WORKSPACE, StorageTarget.USER);
+			this.storageService.store(donotShowWorkspaceRecommendationsStorageKey, true, StorageScope.WORKSPACE, StorageTarget.MACHINE);
 		}
 
 	}
