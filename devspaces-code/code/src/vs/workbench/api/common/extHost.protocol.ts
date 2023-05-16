0ab08bc9eda457cbc85b1d86891487ca1985879b
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-05-16 12:54:37 -0400

    ci: [sync] Update from che-incubator/che-code @ abab52f
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-code/code/src/vs/workbench/api/common/extHost.protocol.ts
+++ devspaces-code/code/src/vs/workbench/api/common/extHost.protocol.ts
@@ -1386,7 +1386,7 @@ export interface IOpenUriOptions {
 }
 
 export interface MainThreadWindowShape extends IDisposable {
-	$getWindowVisibility(): Promise<boolean>;
+	$getInitialState(): Promise<{ isFocused: boolean; isActive: boolean }>;
 	$openUri(uri: UriComponents, uriString: string | undefined, options: IOpenUriOptions): Promise<boolean>;
 	$asExternalUri(uri: UriComponents, options: IOpenUriOptions): Promise<UriComponents>;
 }
@@ -1400,7 +1400,7 @@ export enum CandidatePortSource {
 export interface PortAttributesProviderSelector {
 	pid?: number;
 	portRange?: [number, number];
-	commandMatcher?: RegExp;
+	commandPattern?: RegExp;
 }
 
 export interface MainThreadTunnelServiceShape extends IDisposable {
@@ -2129,6 +2129,7 @@ export interface ExtHostDecorationsShape {
 
 export interface ExtHostWindowShape {
 	$onDidChangeWindowFocus(value: boolean): void;
+	$onDidChangeWindowActive(value: boolean): void;
 }
 
 export interface ExtHostLogLevelServiceShape {
