7ead615eebc5e8d59d220fbb68783fa15e6b6782
Nick Boldt <nboldt@redhat.com>
2023-06-05 13:48:31 -0300

    chore: fix: CRW-4070, CRW-4504 copy org.eclipse.che_checlusters.yaml to devspaces.crd.yaml so that new field gatewayContainer is present (along with other updates)
    Change-Id: I9c8b4b7942a5cfe1abc91c6b48f965918bf517c7
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- devspaces-operator-bundle/build/scripts/sync-che-olm.sh
+++ devspaces-operator-bundle/build/scripts/sync-che-olm.sh
@@ -340,4 +340,8 @@ for CSVFILE in ${TARGETDIR}/manifests/devspaces.csv.yaml; do
 	done
 done
 
+# CRW-4070, CRW-4504 make sure upstream org.eclipse.che_checlusters.yaml content is copied downstream
+# TODO should this be config/crd/bases/org.eclipse.che_checlusters.yaml instead?
+cp "${TARGETDIR}/bundle/${OLM_CHANNEL}/eclipse-che/manifests/org.eclipse.che_checlusters.yaml" "${TARGETDIR}/manifests/devspaces.crd.yaml"
+
 popd >/dev/null || exit
