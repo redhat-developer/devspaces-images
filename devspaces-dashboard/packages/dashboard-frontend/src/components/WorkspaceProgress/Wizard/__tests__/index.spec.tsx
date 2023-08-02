6719d0ee6d8eebc8d8e15565ca4d9a6809bf1c3e
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-08-02 03:30:32 -0400

    ci: [sync] Update from eclipse-che/che-dashboard @ aac2
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

---
+++ devspaces-dashboard/packages/dashboard-frontend/src/components/WorkspaceProgress/Wizard/__tests__/index.spec.tsx
@@ -0,0 +1,142 @@
+/*
+ * Copyright (c) 2018-2023 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+import React from 'react';
+import WorkspaceProgressWizard, { WorkspaceProgressWizardStep } from '..';
+import { Step, StepId } from '../..';
+import getComponentRenderer, { screen } from '../../../../services/__mocks__/getComponentRenderer';
+
+const mockGoToNext = jest.fn();
+
+const { createSnapshot, renderComponent } = getComponentRenderer(getComponent);
+
+describe('WorkspaceProgressWizard', () => {
+  let steps: WorkspaceProgressWizardStep[];
+  let ref: React.RefObject<any>;
+
+  beforeEach(() => {
+    steps = [
+      {
+        id: Step.INITIALIZE,
+        name: Step.INITIALIZE,
+        component: <></>,
+      },
+      {
+        id: Step.LIMIT_CHECK,
+        name: Step.LIMIT_CHECK,
+        component: <></>,
+      },
+      {
+        id: Step.CREATE,
+        name: Step.CREATE,
+        component: <></>,
+        steps: [
+          {
+            id: Step.FETCH,
+            name: Step.FETCH,
+            component: <></>,
+          },
+          {
+            id: Step.CONFLICT_CHECK,
+            name: Step.CONFLICT_CHECK,
+            component: <></>,
+          },
+          {
+            id: Step.APPLY,
+            name: Step.APPLY,
+            component: <></>,
+          },
+        ],
+      },
+    ];
+    ref = React.createRef();
+  });
+
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+
+  test('component snapshot', () => {
+    const activeStepId: StepId = Step.LIMIT_CHECK;
+
+    const snapshot = createSnapshot(activeStepId, steps, ref);
+    expect(snapshot).toMatchSnapshot();
+  });
+
+  test('switching active step', () => {
+    const activeStepId: StepId = Step.INITIALIZE;
+
+    const { reRenderComponent } = renderComponent(activeStepId, steps, ref);
+
+    const buttonInitialize = screen.getByRole('button', { name: Step.INITIALIZE });
+    const buttonLimitCheck = screen.getByRole('button', { name: Step.LIMIT_CHECK });
+
+    expect(buttonInitialize.className.split(' ')).toEqual(expect.arrayContaining(['pf-m-current']));
+    expect(buttonLimitCheck.className.split(' ')).not.toEqual(
+      expect.arrayContaining(['pf-m-current']),
+    );
+
+    const nextActiveStepId = Step.LIMIT_CHECK;
+    reRenderComponent(nextActiveStepId, steps, ref);
+
+    const nextButtonInitialize = screen.getByRole('button', { name: Step.INITIALIZE });
+    const nextButtonLimitCheck = screen.getByRole('button', { name: Step.LIMIT_CHECK });
+
+    expect(nextButtonInitialize.className.split(' ')).not.toEqual(
+      expect.arrayContaining(['pf-m-current']),
+    );
+    expect(nextButtonLimitCheck.className.split(' ')).toEqual(
+      expect.arrayContaining(['pf-m-current']),
+    );
+  });
+
+  describe('trigger goToNext using reference', () => {
+    test('on the very first step', () => {
+      const activeStepId: StepId = Step.INITIALIZE;
+
+      renderComponent(activeStepId, steps, ref);
+
+      expect(mockGoToNext).not.toHaveBeenCalled();
+
+      ref.current?.goToNext();
+
+      expect(mockGoToNext).toHaveBeenCalledWith(Step.LIMIT_CHECK, Step.INITIALIZE);
+    });
+
+    test('on the very last step', () => {
+      const activeStepId: StepId = Step.APPLY;
+
+      renderComponent(activeStepId, steps, ref);
+
+      expect(mockGoToNext).not.toHaveBeenCalled();
+
+      ref.current?.goToNext();
+
+      expect(mockGoToNext).toHaveBeenCalledWith(undefined, Step.APPLY);
+    });
+  });
+});
+
+function getComponent(
+  activeStepId: StepId,
+  steps: WorkspaceProgressWizardStep[],
+  ref: React.RefObject<any>,
+): React.ReactElement {
+  return (
+    <WorkspaceProgressWizard
+      ref={ref}
+      activeStepId={activeStepId}
+      steps={steps}
+      onNext={mockGoToNext}
+    />
+  );
+}
---
+++ devspaces-dashboard/packages/dashboard-frontend/src/components/WorkspaceProgress/Wizard/index.tsx
@@ -0,0 +1,198 @@
+/*
+ * Copyright (c) 2018-2023 Red Hat, Inc.
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ *
+ * Contributors:
+ *   Red Hat, Inc. - initial API and implementation
+ */
+
+import * as PF from '@patternfly/react-core';
+import wizardStyles from '@patternfly/react-styles/css/components/Wizard/wizard';
+import React from 'react';
+import { StepId } from '..';
+import styles from './index.module.css';
+
+export interface WorkspaceProgressWizardStep extends PF.WizardStep {
+  id: StepId;
+  steps?: WorkspaceProgressWizardStep[];
+}
+
+export type Props = {
+  activeStepId: StepId;
+  steps: WorkspaceProgressWizardStep[];
+  onNext: (nextStepId: StepId | undefined, prevStepId: StepId) => void;
+};
+
+class WorkspaceProgressWizard extends React.Component<Props> {
+  private handleGoToStepById() {
+    console.warn('Not implemented: handleGoToStepById');
+    return false;
+  }
+
+  private handleGoToStepByName() {
+    console.warn('Not implemented: handleGoToStepByName');
+    return false;
+  }
+
+  private handleBack() {
+    console.warn('Not implemented: handleBack');
+    return false;
+  }
+
+  private handleClose() {
+    console.warn('Not implemented: handleClose');
+    return false;
+  }
+
+  public goToNext(): void {
+    const flattenedSteps = this.flattenSteps(this.props.steps);
+    // find the index of the next after the current active step
+    const activeStepNumber = this.getFlattenedStepsNumber(flattenedSteps, this.props.activeStepId);
+
+    if (activeStepNumber === 0) {
+      // current step is not found by its id, do nothing
+      return;
+    }
+
+    if (activeStepNumber === flattenedSteps.length) {
+      // last step
+      this.props.onNext(undefined, this.props.activeStepId);
+      return;
+    }
+
+    const nextStep = flattenedSteps[activeStepNumber];
+    this.props.onNext(nextStep.id, this.props.activeStepId);
+  }
+
+  private handleNavToggle() {
+    console.warn('Not implemented: handleNavToggle');
+    return false;
+  }
+
+  private setDefaultValues(steps: WorkspaceProgressWizardStep[]): WorkspaceProgressWizardStep[] {
+    const withDefaults = (step: WorkspaceProgressWizardStep) =>
+      Object.assign({ canJumpTo: true }, step);
+
+    return steps.map(step => {
+      const subSteps = step.steps;
+      if (subSteps !== undefined) {
+        subSteps.map(subStep => withDefaults(subStep));
+      }
+      return withDefaults(step);
+    });
+  }
+
+  private flattenSteps(steps: WorkspaceProgressWizardStep[]): WorkspaceProgressWizardStep[] {
+    return steps.reduce((acc, step) => {
+      if (step.steps) {
+        return acc.concat(step.steps);
+      }
+      return acc.concat(step);
+    }, [] as WorkspaceProgressWizardStep[]);
+  }
+
+  /**
+   * Returns the number (index + 1) of the step in the flattened steps array
+   */
+  private getFlattenedStepsNumber(
+    flattenedSteps: WorkspaceProgressWizardStep[],
+    stepId: StepId,
+  ): number {
+    return flattenedSteps.findIndex(step => step.id === stepId) + 1;
+  }
+
+  private buildWizardNav(
+    activeStepId: StepId,
+    steps: WorkspaceProgressWizardStep[],
+  ): React.ReactElement {
+    const stepsWithDefaults = this.setDefaultValues(steps);
+    const flattenedSteps = this.flattenSteps(stepsWithDefaults);
+
+    return (
+      <PF.WizardNav aria-label="Workspace Progress Steps" isOpen={true}>
+        {stepsWithDefaults.map(step => {
+          const { canJumpTo, name, steps = [], id } = step;
+          const flattenedStepNumber = this.getFlattenedStepsNumber(flattenedSteps, id);
+
+          const hasActiveChild = steps.some(subStep => subStep.id === activeStepId);
+
+          return (
+            <PF.WizardNavItem
+              key={id}
+              content={name}
+              step={flattenedStepNumber}
+              isCurrent={activeStepId === id || hasActiveChild}
+              isDisabled={!canJumpTo}
+              onNavItemClick={() => this.handleGoToStepById()}
+            >
+              {steps.length !== 0 && (
+                <PF.WizardNav returnList>
+                  {steps.map(subStep => {
+                    if (subStep.isFinishedStep) {
+                      return;
+                    }
+
+                    const { canJumpTo, name, id } = subStep;
+                    const flattenedStepNumber = this.getFlattenedStepsNumber(flattenedSteps, id);
+                    return (
+                      <PF.WizardNavItem
+                        key={id}
+                        content={name}
+                        step={flattenedStepNumber}
+                        isCurrent={activeStepId === id}
+                        isDisabled={!canJumpTo}
+                        onNavItemClick={() => this.handleGoToStepById()}
+                      />
+                    );
+                  })}
+                </PF.WizardNav>
+              )}
+            </PF.WizardNavItem>
+          );
+        })}
+      </PF.WizardNav>
+    );
+  }
+
+  render() {
+    const { activeStepId, steps } = this.props;
+
+    const hasNoBodyPadding = false;
+
+    const activeStep = steps[0];
+
+    return (
+      <PF.WizardContextProvider
+        value={{
+          activeStep: activeStep,
+          goToStepById: () => this.handleGoToStepById(),
+          goToStepByName: () => this.handleGoToStepByName(),
+          onBack: () => this.handleBack(),
+          onClose: () => this.handleClose(),
+          onNext: () => this.goToNext(),
+        }}
+      >
+        <div className={`${styles.progress} ${wizardStyles.wizard}`}>
+          <PF.WizardToggle
+            activeStep={activeStep}
+            hasNoBodyPadding={hasNoBodyPadding}
+            isNavOpen={true}
+            nav={() => this.buildWizardNav(activeStepId, steps)}
+            onNavToggle={() => this.handleNavToggle()}
+            steps={[]}
+          >
+            <React.Fragment />
+          </PF.WizardToggle>
+        </div>
+      </PF.WizardContextProvider>
+    );
+  }
+}
+
+export default React.forwardRef<WorkspaceProgressWizard, Props>((props, ref) => (
+  <WorkspaceProgressWizard ref={ref} {...props} />
+));
