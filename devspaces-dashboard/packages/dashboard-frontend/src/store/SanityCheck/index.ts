6ac6e4c80ba2150927ef9f14a9a7e7ecc5860b77
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-09-08 13:02:48 +0000

    ci: [sync] Update from eclipse-che/che-dashboard @ f0ef0
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/packages/dashboard-frontend/src/store/SanityCheck/index.ts
+++ devspaces-dashboard/packages/dashboard-frontend/src/store/SanityCheck/index.ts
@@ -12,6 +12,7 @@
 
 import { Action, Reducer } from 'redux';
 import { AppThunk } from '..';
+import { helpers } from '@eclipse-che/common';
 import { container } from '../../inversify.config';
 import { getDefer } from '../../services/helpers/deferred';
 import { delay } from '../../services/helpers/delay';
@@ -118,6 +119,14 @@ export const actionCreators: ActionCreators = {
           type: Type.RECEIVED_BACKEND_CHECK_ERROR,
           error: errorMessage,
         });
+        console.error(helpers.errors.getMessage(e));
+        if (
+          helpers.errors.includesAxiosResponse(e) &&
+          e.response.data.trace &&
+          Array.isArray(e.response.data.trace)
+        ) {
+          console.error(e.response.data.trace.join('\n'));
+        }
         throw new Error(errorMessage);
       }
     },
