467506417c4f8402eac8fed04adcd0999757c5c8
Nick Boldt <nboldt@redhat.com>
2023-09-22 14:58:01 -0300

    add copyright, link to issues/more info
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- devspaces-traefik/build/scripts/checkgoMod.sh
+++ devspaces-traefik/build/scripts/checkgoMod.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
-# rules for this build: cannot use hashicorp/consul > 1.15.5
+#
+# Copyright (c) 2023 Red Hat, Inc.
+# This program and the accompanying materials are made
+# available under the terms of the Eclipse Public License 2.0
+# which is available at https://www.eclipse.org/legal/epl-2.0/
+#
+# SPDX-License-Identifier: EPL-2.0
+#
+# Contributors:
+#   Red Hat, Inc. - initial API and implementation
+#
+# set rules for this build's go.mod file: cannot use dependencies > set limit
+# see https://issues.redhat.com/browse/CRW-4868
+# see https://github.com/cncf/foundation/issues/617
 
 if [[ -f $1/go.mod ]]; then 
     goMod="$1/go.mod"
@@ -9,31 +22,32 @@ else
 fi
 
 declare -A limits
+declare -A links
 limits["hashicorp/consul"]="v1.15.5"
-# limits["hashicorp/consul/api"]="v1.15"
-
+links["hashicorp/consul"]="https://github.com/cncf/foundation/issues/617 and https://issues.redhat.com/browse/CRW-4868"
+# add other array values here if there are more things to limit
 
 checkVersion() {
-  # $1 - upper limit version
-  # $2 - actual version
-  # $3 - versioned thing 
-  if [[  "$1" = "$(echo -e "$1\n$2" | sort -rV | head -n1)" ]]; then
-    # thing realversion <= upperlimitversion
-	echo "[INFO] $3 version $2 <= $1: OK"
-	true
-  else 
-	echo "[ERROR] $3 version $2 not allowed. Require $3 version <= $1"
-	exit 2
-  fi
+    # $1 - upper limit version
+    # $2 - actual version
+    # $3 - versioned thing
+    # #4 - link for more info
+    if [[ "$1" = "$(echo -e "$1\n$2" | sort -rV | head -n1)" ]]; then
+        # thing realversion <= upperlimitversion
+        echo "[INFO] $3 version $2 <= $1: OK"
+        true
+    else 
+        echo "[ERROR] $3 version $2 is not allowed. Must use $3 <= $1"
+        echo "[ERROR] See $4 for more info"
+        exit 2
+    fi
 }
 
 echo "[INFO] Checking go.mod requirement upper limits..."
 for i in "${!limits[@]}"; do
-
     lines="$(grep "$i " "$goMod")"
     for line in "$lines"; do
         version=$(echo "$line" | sed -r -e "s#.+$i ##"); # echo "Got version = $version"
-        checkVersion ${limits[$i]} $version $i
+        checkVersion ${limits[$i]} $version $i "${links[$i]}"
     done
-
 done
