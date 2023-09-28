b68ee52d136591b0b9a4dd54f75fc07335787729
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-09-28 18:52:40 +0000

    ci: [sync] Update from che-incubator/configbump @ b407
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-configbump/cmd/configbump/main.go
+++ devspaces-configbump/cmd/configbump/main.go
@@ -35,7 +35,7 @@ type opts struct {
 
 // Version returns the version of the program
 func (opts) Version() string {
-	return "config-bump 7.75.0-next"
+	return "config-bump 7.76.0-next"
 }
 
 const controllerName = "config-bump"
