2ee61ac9e92cb84c94a7fc313a64dacbd442d04c
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-10-05 03:33:40 -0400

    ci: [sync] Update from che-incubator/che-code @ 46a8eb
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

---
+++ devspaces-code/code/extensions/che-remote/src/axios-certificate-authority.ts
@@ -0,0 +1,75 @@
+/**********************************************************************
+ * Copyright (c) 2023 Red Hat, Inc.
+ *
+ * This program and the accompanying materials are made
+ * available under the terms of the Eclipse Public License 2.0
+ * which is available at https://www.eclipse.org/legal/epl-2.0/
+ *
+ * SPDX-License-Identifier: EPL-2.0
+ ***********************************************************************/
+
+/* eslint-disable header/header */
+
+import * as axios from 'axios';
+import * as fs from 'fs-extra';
+import * as https from 'https';
+import * as path from 'path';
+
+const DEFAULT_CHE_SELF_SIGNED_MOUNT_PATH = '/public-certs';
+const CHE_SELF_SIGNED_MOUNT_PATH = process.env.CHE_SELF_SIGNED_MOUNT_PATH;
+
+const certificateAuthority = getCertificateAuthority(
+	CHE_SELF_SIGNED_MOUNT_PATH ? CHE_SELF_SIGNED_MOUNT_PATH : DEFAULT_CHE_SELF_SIGNED_MOUNT_PATH,
+);
+
+export const axiosInstance = certificateAuthority
+	? axios.default.create({
+		httpsAgent: new https.Agent({
+			ca: certificateAuthority,
+		}),
+	})
+	: axios.default;
+
+function getCertificateAuthority(certPath: string): Buffer[] | undefined {
+	if (!fs.existsSync(certPath)) {
+		return undefined;
+	}
+
+	const certificateAuthority: Buffer[] = [];
+	searchCertificate(certPath, certificateAuthority);
+
+	return certificateAuthority.length > 0 ? certificateAuthority : undefined;
+}
+
+function searchCertificate(
+	certPath: string,
+	certificateAuthority: Buffer[],
+	subdirLevel = 1,
+): void {
+	const maxSubdirQuantity = 10;
+	const maxSubdirLevel = 5;
+
+	const tmpPaths: string[] = [];
+	try {
+		const publicCertificates = fs.readdirSync(certPath);
+		for (const publicCertificate of publicCertificates) {
+			const newPath = path.join(certPath, publicCertificate);
+			if (fs.lstatSync(newPath).isDirectory()) {
+				if (tmpPaths.length < maxSubdirQuantity) {
+					tmpPaths.push(newPath);
+				}
+			} else {
+				const fullPath = path.join(certPath, publicCertificate);
+				certificateAuthority.push(fs.readFileSync(fullPath));
+			}
+		}
+	} catch (e) {
+		// no-op
+	}
+
+	if (subdirLevel < maxSubdirLevel) {
+		for (const path of tmpPaths) {
+			searchCertificate(path, certificateAuthority, ++subdirLevel);
+		}
+	}
+}
--- devspaces-code/code/extensions/che-remote/src/extension.ts
+++ devspaces-code/code/extensions/che-remote/src/extension.ts
@@ -12,9 +12,9 @@
 
 // local import to find the correct type
 import { Main as DevWorkspaceGenerator } from '@eclipse-che/che-devworkspace-generator/lib/main';
-import * as axios from 'axios';
 import * as fs from 'fs-extra';
 import * as vscode from 'vscode';
+import { axiosInstance } from './axios-certificate-authority';
 
 const DEVFILE_NAME = 'devfile.yaml';
 const DOT_DEVFILE_NAME = '.devfile.yaml';
@@ -117,7 +117,10 @@ async function updateDevfile(cheApi: any): Promise<void> {
 
   const currentDevfile = await devfileService.get();
   const projects = currentDevfile.projects || [];
-  const newContent = await devWorkspaceGenerator.generateDevfileContext({ devfilePath, editorContent: EDITOR_CONTENT_STUB, projects: [] }, axios.default);
+  const pluginRegistryUrl = process.env.CHE_PLUGIN_REGISTRY_INTERNAL_URL;
+  
+  console.info(`Using ${pluginRegistryUrl} to generate a new Devfile Context`);
+  const newContent = await devWorkspaceGenerator.generateDevfileContext({ devfilePath, editorContent: EDITOR_CONTENT_STUB, pluginRegistryUrl, projects: [] }, axiosInstance);
   if (newContent) {
     newContent.devWorkspace.spec!.template!.projects = projects;
     await devfileService.updateDevfile(newContent.devWorkspace.spec?.template);
