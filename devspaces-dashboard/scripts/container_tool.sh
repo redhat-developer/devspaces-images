ba0554d36e35437ea48732ff1bc0bb2be25eb055
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-10-16 07:28:30 +0000

    ci: [sync] Update from eclipse-che/che-dashboard @ 6811f
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/scripts/container_tool.sh
+++ devspaces-dashboard/scripts/container_tool.sh
@@ -1,40 +1,50 @@
 #!/bin/bash
 
-# check if command exists
-check_command() {
-    if command -v "$1" &> /dev/null; then
-        return 0 # not found
-    else
-        return 1 # found
-    fi
+# Function to check if a command is available
+command_exists() {
+    command -v "$1" >/dev/null 2>&1
 }
+
+# Check for Podman
+if command_exists "podman"; then
+    # Check if Podman machine is running
+    if podman info &>/dev/null; then
+        container_engine="podman"
+    fi
+fi
+
+# Check for Docker
+if command_exists "docker"; then
+    # Check if Docker daemon is running
+    if docker info &>/dev/null; then
+        container_engine="docker"
+    fi
+fi
+
+# If neither Podman nor Docker is found or running
+if [ -z "$container_engine" ]; then
+    echo "Neither Podman nor Docker is installed or running."
+    exit 1
+fi
 
 # Run command using Docker or Podman whichever is available
 container_tool() {
     local command=$1
     shift
 
-    if check_command "podman"; then
-        CONTAINER_TOOL="podman"
-    elif check_command "docker"; then
-        CONTAINER_TOOL="docker"
-    else
-        echo "Error: Docker or Podman not found on the system."
-        exit 1
-    fi
-
-    "$CONTAINER_TOOL" "$command" "$@"
+    echo "Container engine: $container_engine"
+    "$container_engine" "$command" "$@"
 }
 
 # Main script
 case "$1" in
-    build|run|push)
-        container_tool "$@"
-        ;;
-    *)
-        echo "Uknown command. Use: build, run or push."
-        exit 1
-        ;;
+build | run | push)
+    container_tool "$@"
+    ;;
+*)
+    echo "Unknown command. Use: build, run, or push."
+    exit 1
+    ;;
 esac
 
 exit 0
