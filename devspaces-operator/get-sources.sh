2bb9be8ce8b788c343ddbacd432794a24e6d1263
Nick Boldt <nboldt@redhat.com>
2023-10-18 17:57:03 -0300

    chore: CRW-4821 partial backport of switch to use brew instead of rhpkg for container-build commands
    Signed-off-by: Nick Boldt <nboldt@redhat.com>

--- devspaces-operator/get-sources.sh
+++ devspaces-operator/get-sources.sh
@@ -24,7 +24,12 @@ done
 if [[ ${forceBuild} -eq 1 ]] || [[ ${doRhpkgContainerBuild} -eq 1 ]]; then
 	echo "[INFO] #0 Trigger container-build in current branch: rhpkg container-build ${scratchFlag} ${targetFlag}"
 	git status || true
-	tmpfile=$(mktemp) && rhpkg container-build ${scratchFlag} ${targetFlag} --nowait | tee 2>&1 "${tmpfile}"
+		gitbranch="$(git rev-parse --abbrev-ref HEAD)"
+		if [[ $scratchFlag == "--scratch" ]]; then gitbranch="devspaces-3-rhel-8"; fi
+		target=${gitbranch}-containers-candidate
+		repo="$(git remote -v | grep origin | head -1 | sed -r -e "s#.+/containers/(.+) \(fetch.+#\1#")"
+		sha="$(git rev-parse HEAD)"
+		tmpfile=$(mktemp) && brew container-build ${target} git+https://pkgs.devel.redhat.com/git/containers/${repo}#${sha} --git-branch ${gitbranch} --nowait 2>/dev/null | tee 2>&1 "${tmpfile}"
 	taskID=$(cat "${tmpfile}" | grep "Created task:" | sed -e "s#Created task:##") && brew watch-logs $taskID | tee 2>&1 "${tmpfile}"
 	ERRORS="$(grep "image build failed" "${tmpfile}")" && rm -f "${tmpfile}"
 	if [[ "$ERRORS" != "" ]]; then echo "Brew build has failed:
