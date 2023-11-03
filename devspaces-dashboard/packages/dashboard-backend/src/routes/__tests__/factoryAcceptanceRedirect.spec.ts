de4e4c64cdc539259ce108056195b14162618261
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-11-03 15:15:58 -0400

    ci: [sync] Update from eclipse-che/che-dashboard @ e056
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/packages/dashboard-backend/src/routes/__tests__/factoryAcceptanceRedirect.spec.ts
+++ devspaces-dashboard/packages/dashboard-backend/src/routes/__tests__/factoryAcceptanceRedirect.spec.ts
@@ -33,7 +33,7 @@ describe('Factory Acceptance Redirect', () => {
     expect(res.headers.location).toEqual(`/dashboard/#/load-factory?url=${factoryUrl}`);
   });
 
-  it('should redirect "/f?url=factoryUrl"', async () => {
+  it('should redirect "/f?factoryLink=url%3DfactoryUrl"', async () => {
     const factoryUrl = 'factoryUrl';
     const res = await app.inject({
       url: `/f?factoryLink=${encodeURIComponent('url=' + factoryUrl)}`,
@@ -42,32 +42,65 @@ describe('Factory Acceptance Redirect', () => {
     expect(res.headers.location).toEqual(`/dashboard/#/load-factory?url=${factoryUrl}`);
   });
 
-  it('should redirect "/dashboard/f?url=factoryUrl"', async () => {
+  it('should redirect "/f?factoryLink=che-editor=che-incubator/.che-code/insiders&override.devfileFilename=my.devfile.yaml&url=factoryUr"', async () => {
     const factoryUrl = 'factoryUrl';
     const res = await app.inject({
-      url: `/dashboard/f?url=${factoryUrl}`,
+      url: `/f?${encodeURIComponent(
+        'factoryLink=che-editor=che-incubator/.che-code/insiders&override.devfileFilename=my.devfile.yaml&url=factoryUrl',
+      )}`,
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual(
+      `/dashboard/#/load-factory?che-editor=che-incubator%2F.che-code%2Finsiders&override.devfileFilename=my.devfile.yaml&url=${factoryUrl}`,
+    );
+  });
+
+  it('should redirect "/f?url=factoryUrl"', async () => {
+    const factoryUrl = 'factoryUrl';
+    const res = await app.inject({
+      url: `/f?url=${factoryUrl}`,
     });
     expect(res.statusCode).toEqual(302);
     expect(res.headers.location).toEqual(`/dashboard/#/load-factory?url=${factoryUrl}`);
   });
 
-  it('should redirect "/dashboard/f?factoryLink%3Durl%3DfactoryUrl"', async () => {
+  it('should redirect "/f?factoryLink%3Durl%3DfactoryUrl"', async () => {
     const factoryUrl = 'factoryUrl';
     const res = await app.inject({
-      url: `/dashboard/f?factoryLink%3Durl%3D${factoryUrl}`,
+      url: `/f?factoryLink%3Durl%3D${factoryUrl}`,
     });
     expect(res.statusCode).toEqual(302);
     expect(res.headers.location).toEqual(`/dashboard/#/load-factory?url=${factoryUrl}`);
   });
 
-  it('should redirect "/dashboard/f?factoryLink%3Durl%3DfactoryUrl%26error_code%3Daccess_denied"', async () => {
+  it('should redirect "/f?factoryLink%3Durl%3DfactoryUrl%26error_code%3Daccess_denied"', async () => {
     const factoryUrl = 'factoryUrl';
     const res = await app.inject({
-      url: `/dashboard/f?factoryLink%3Durl%3D${factoryUrl}%26error_code%3Daccess_denied`,
+      url: `/f?factoryLink%3Durl%3D${factoryUrl}%26error_code%3Daccess_denied`,
     });
     expect(res.statusCode).toEqual(302);
     expect(res.headers.location).toEqual(
       `/dashboard/#/load-factory?url=${factoryUrl}&error_code=access_denied`,
+    );
+  });
+
+  it('should redirect "/f?factoryLink=url%3Dhttps%253A%252F%252Fgithub.com%252Folexii4%252Fhelloworld.git"', async () => {
+    const res = await app.inject({
+      url: `/f?factoryLink=url%3Dhttps%253A%252F%252Fgithub.com%252Folexii4%252Fhelloworld.git`,
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual(
+      `/dashboard/#/load-factory?url=https%3A%2F%2Fgithub.com%2Folexii4%2Fhelloworld.git`,
+    );
+  });
+
+  it('should redirect "/f?factoryLink=override.devfileFilename%3Ddevfile2.yaml%26url%3Dhttps%253A%252F%252Fgithub.com%252Folexii4%252Fhelloworld.git"', async () => {
+    const res = await app.inject({
+      url: `/f?factoryLink=override.devfileFilename%3Ddevfile2.yaml%26url%3Dhttps%253A%252F%252Fgithub.com%252Folexii4%252Fhelloworld.git`,
+    });
+    expect(res.statusCode).toEqual(302);
+    expect(res.headers.location).toEqual(
+      `/dashboard/#/load-factory?override.devfileFilename=devfile2.yaml&url=https%3A%2F%2Fgithub.com%2Folexii4%2Fhelloworld.git`,
     );
   });
 });
