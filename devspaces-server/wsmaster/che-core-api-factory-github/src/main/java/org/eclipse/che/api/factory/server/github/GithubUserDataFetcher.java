512ded40281e72f0b9146bfc0e7382348cdcebb2
devstudio-release <nickboldt+devstudio-release@gmail.com>
2023-11-14 14:50:14 +0000

    ci: [sync] Update from eclipse-che/che-server @ 10df3
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-server/wsmaster/che-core-api-factory-github/src/main/java/org/eclipse/che/api/factory/server/github/GithubUserDataFetcher.java
+++ devspaces-server/wsmaster/che-core-api-factory-github/src/main/java/org/eclipse/che/api/factory/server/github/GithubUserDataFetcher.java
@@ -11,34 +11,16 @@
  */
 package org.eclipse.che.api.factory.server.github;
 
-import com.google.common.base.Joiner;
-import com.google.common.collect.ImmutableSet;
-import java.util.Set;
 import javax.inject.Inject;
 import javax.inject.Named;
-import org.eclipse.che.api.auth.shared.dto.OAuthToken;
-import org.eclipse.che.api.factory.server.scm.AbstractGitUserDataFetcher;
-import org.eclipse.che.api.factory.server.scm.GitUserData;
-import org.eclipse.che.api.factory.server.scm.PersonalAccessToken;
 import org.eclipse.che.api.factory.server.scm.PersonalAccessTokenManager;
-import org.eclipse.che.api.factory.server.scm.exception.ScmBadRequestException;
-import org.eclipse.che.api.factory.server.scm.exception.ScmCommunicationException;
-import org.eclipse.che.api.factory.server.scm.exception.ScmItemNotFoundException;
 import org.eclipse.che.commons.annotation.Nullable;
 import org.eclipse.che.security.oauth.OAuthAPI;
 
 /** GitHub user data retriever. */
-public class GithubUserDataFetcher extends AbstractGitUserDataFetcher {
-  private final String apiEndpoint;
-  /** GitHub API client. */
-  private final GithubApiClient githubApiClient;
-
+public class GithubUserDataFetcher extends AbstractGithubUserDataFetcher {
   /** Name of this OAuth provider as found in OAuthAPI. */
   private static final String OAUTH_PROVIDER_NAME = "github";
-
-  /** Collection of OAuth scopes required to make integration with GitHub work. */
-  public static final Set<String> DEFAULT_TOKEN_SCOPES =
-      ImmutableSet.of("repo", "user:email", "read:user");
 
   @Inject
   public GithubUserDataFetcher(
@@ -46,45 +28,24 @@ public class GithubUserDataFetcher extends AbstractGitUserDataFetcher {
       @Nullable @Named("che.integration.github.oauth_endpoint") String oauthEndpoint,
       OAuthAPI oAuthTokenFetcher,
       PersonalAccessTokenManager personalAccessTokenManager) {
-    this(
+    super(
         apiEndpoint,
         oAuthTokenFetcher,
         personalAccessTokenManager,
-        new GithubApiClient(oauthEndpoint));
+        new GithubApiClient(oauthEndpoint),
+        OAUTH_PROVIDER_NAME);
   }
 
-  /** Constructor used for testing only. */
-  public GithubUserDataFetcher(
+  GithubUserDataFetcher(
       String apiEndpoint,
       OAuthAPI oAuthTokenFetcher,
       PersonalAccessTokenManager personalAccessTokenManager,
       GithubApiClient githubApiClient) {
-    super(OAUTH_PROVIDER_NAME, personalAccessTokenManager, oAuthTokenFetcher);
-    this.githubApiClient = githubApiClient;
-    this.apiEndpoint = apiEndpoint;
-  }
-
-  @Override
-  protected GitUserData fetchGitUserDataWithOAuthToken(OAuthToken oAuthToken)
-      throws ScmItemNotFoundException, ScmCommunicationException, ScmBadRequestException {
-    GithubUser user = githubApiClient.getUser(oAuthToken.getToken());
-    return new GitUserData(user.getName(), user.getEmail());
-  }
-
-  @Override
-  protected GitUserData fetchGitUserDataWithPersonalAccessToken(
-      PersonalAccessToken personalAccessToken)
-      throws ScmItemNotFoundException, ScmCommunicationException, ScmBadRequestException {
-    GithubUser user = githubApiClient.getUser(personalAccessToken.getToken());
-    return new GitUserData(user.getName(), user.getEmail());
-  }
-
-  protected String getLocalAuthenticateUrl() {
-    return apiEndpoint
-        + "/oauth/authenticate?oauth_provider="
-        + OAUTH_PROVIDER_NAME
-        + "&scope="
-        + Joiner.on(',').join(DEFAULT_TOKEN_SCOPES)
-        + "&request_method=POST&signature_method=rsa";
+    super(
+        apiEndpoint,
+        oAuthTokenFetcher,
+        personalAccessTokenManager,
+        githubApiClient,
+        OAUTH_PROVIDER_NAME);
   }
 }
