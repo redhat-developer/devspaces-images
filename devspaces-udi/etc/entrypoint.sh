1aa65ca1388cc1916a17b95f3666e67e73b40203
Andrew O <andrew@aobuchow.com>
2023-11-21 13:20:53 -0500

    Move tools & configs from /home/user/ to /home/tooling/ (#525)
    * Use bash-completions package
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Don't modify ~/.bashrc, use /etc/profile.d/ instead
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Build stow from sources
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Move tools & configs from /home/user/ to /home/tooling/
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Fix for multiple $KUBEDOCK_PARAMS
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Install vim
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Don't stow .viminfo
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Ensure ~/.bashrc and ~/.bash_profile exist when persistUserHome enabled
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Remove obsolete bash prompt creation from UDI entrypoint
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Don't fail entrypoint if kubeconfig could not be found
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * chore: define user directory for binaries
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Ensure mountpoint test doesn't cause entrypoint to fail
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Update devspaces-udi/etc/entrypoint.sh
    
    Co-authored-by: Valerii Svydenko <vsvydenk@redhat.com>
    
    * Remove redundant creation of Go bin directory
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Remove redundant /home/tooling/ creation
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Pre-create common tooling config directories
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Update devspaces-udi/etc/entrypoint.sh
    
    Co-authored-by: Angel Misevski <amisevsk@redhat.com>
    
    * Disable code ready builder repo after building stow
    
    We disable the code ready builder repo on each architecture to prevent
    accidentally pulling in the wrong packages. For example, the maven-resolver
    package differs between the rhel 8 app stream repo & the code ready builder repo
    which leads to maven being unable to download artifacts.
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Fix for incorrect python module
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Update devspaces-udi/Dockerfile
    
    Co-authored-by: Angel Misevski <amisevsk@redhat.com>
    
    * Update devspaces-udi/Dockerfile
    
    Co-authored-by: Angel Misevski <amisevsk@redhat.com>
    
    * Ensure PATH contains /home/user/ and /home/tooling/ entries
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * fixup! Disable code ready builder repo after building stow
    
    * Create ~/.java/current/ symbolic links in /home/tooling/ instead of /home/user/
    
    In order for stow in the UDI's entrypoint to complete successfully,
    any files present in /home/user/ (that are not in the .stow-local-ignore)
    must not also exist in /home/tooling/, otherwise a conflict would occur.
    
    Since the symbolic link creation from JAVA_HOME_8/11/17 occurs before stow
    is executed, the symbolic link must be created from JAVA_HOME_8/11/17 -> /home/tooling/
    to prevent a stow conflict.
    
    Stow will then create a symbolic link from
    /home/tooling/.java/current/ -> /home/user/.java/current/ so that JAVA_HOME remains valid.
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    
    * Update devspaces-udi/Dockerfile
    
    * Update devspaces-udi/Dockerfile
    
    * fixup! Disable code ready builder repo after building stow
    
    ---------
    
    Signed-off-by: Andrew Obuchowicz <aobuchow@redhat.com>
    Co-authored-by: Nick Boldt <nboldt@redhat.com>
    Co-authored-by: Valerii Svydenko <vsvydenk@redhat.com>
    Co-authored-by: Angel Misevski <amisevsk@redhat.com>

--- devspaces-udi/etc/entrypoint.sh
+++ devspaces-udi/etc/entrypoint.sh
@@ -43,30 +43,23 @@ if [ -f "${HOME}"/.venv/bin/activate ]; then
 fi
 
 #############################################################################
-# Setup $PS1 for a consistent and reasonable prompt
-#############################################################################
-if [ -w "${HOME}" ] && [ ! -f "${HOME}"/.bashrc ]; then
-  echo "PS1='[\u@\h \W]\$ '" >> "${HOME}"/.bashrc
-fi
-
-#############################################################################
 # use java 8 if USE_JAVA8 is set to 'true', 
 # use java 17 if USE_JAVA17 is set to 'true', 
 # by default it is java 11
 #############################################################################
-rm -rf "${HOME}"/.java/current
-mkdir -p "${HOME}"/.java/current
+rm -rf /home/tooling/.java/current
+mkdir -p /home/tooling/.java/current
 if [ "${USE_JAVA8}" == "true" ] && [ ! -z "${JAVA_HOME_8}" ]; then
-  ln -s "${JAVA_HOME_8}"/* "${HOME}"/.java/current
+  ln -s "${JAVA_HOME_8}"/* /home/tooling/.java/current
   echo "Java environment set to ${JAVA_HOME_8}"
 elif [ "${USE_JAVA17}" == "true" ] && [ ! -z "${JAVA_HOME_17}" ]; then
-  ln -s "${JAVA_HOME_17}"/* "${HOME}"/.java/current
+  ln -s "${JAVA_HOME_17}"/* /home/tooling/.java/current
   echo "Java environment set to ${JAVA_HOME_17}"
 else
   # End of Support for OpenJDK 11 in October 2024
   # https://access.redhat.com/articles/1299013
   echo "TODO: it needs to set JDK 17 as default before OpenJDK 11 hits EOL in October 2024"
-  ln -s "${JAVA_HOME_11}"/* "${HOME}"/.java/current
+  ln -s "${JAVA_HOME_11}"/* /home/tooling/.java/current
   echo "Java environment set to ${JAVA_HOME_11}"
 fi
 
@@ -83,41 +76,73 @@ if [[ "${KUBEDOCK_ENABLED:-false}" == "true" ]]; then
     echo "Kubedock is enabled (env variable KUBEDOCK_ENABLED is set to true)."
 
     SECONDS=0
-    until [ -f /home/user/.kube/config ]; do
-        if (( SECONDS > 10 )); then
-            echo "Giving up..."
-            exit 1
+    KUBEDOCK_TIMEOUT=${KUBEDOCK_TIMEOUT:-10}
+    until [ -f $KUBECONFIG ]; do
+        if (( SECONDS > KUBEDOCK_TIMEOUT )); then
+            break
         fi
         echo "Kubeconfig doesn't exist yet. Waiting..."
         sleep 1
     done
-    echo "Kubeconfig found."
 
-    KUBEDOCK_PARAMS=${KUBEDOCK_PARAMS:-"--reverse-proxy"}
+    if [ -f $KUBECONFIG ]; then
+      echo "Kubeconfig found."
 
-    echo "Starting kubedock with params \"${KUBEDOCK_PARAMS}\"..."
-    
-    kubedock server "${KUBEDOCK_PARAMS}" > /tmp/kubedock.log 2>&1 &
-    
-    echo "Done."
+      KUBEDOCK_PARAMS=${KUBEDOCK_PARAMS:-"--reverse-proxy --kubeconfig $KUBECONFIG"}
 
-    echo "Replacing podman with podman-wrapper.sh..."
+      echo "Starting kubedock with params \"${KUBEDOCK_PARAMS}\"..."
 
-    mkdir -p /home/user/.local/bin/
-    ln -f -s /usr/bin/podman-wrapper.sh /home/user/.local/bin/podman
+      kubedock server ${KUBEDOCK_PARAMS} > /tmp/kubedock.log 2>&1 &
 
-    export TESTCONTAINERS_RYUK_DISABLED="true"
-    export TESTCONTAINERS_CHECKS_DISABLE="true"
+      echo "Done."
 
-    echo "Done."
-    echo
+      echo "Replacing podman with podman-wrapper.sh..."
+
+      ln -f -s /usr/bin/podman-wrapper.sh /home/tooling/.local/bin/podman
+
+      export TESTCONTAINERS_RYUK_DISABLED="true"
+      export TESTCONTAINERS_CHECKS_DISABLE="true"
+
+      echo "Done."
+      echo
+    else
+        echo "Could not find Kubeconfig at $KUBECONFIG"
+        echo "Giving up..."
+    fi
 else
     echo
     echo "Kubedock is disabled. It can be enabled with the env variable \"KUBEDOCK_ENABLED=true\""
     echo "set in the workspace Devfile or in a Kubernetes ConfigMap in the developer namespace."
     echo
-    mkdir -p /home/user/.local/bin/
-    ln -f -s /usr/bin/podman.orig /home/user/.local/bin/podman
+    ln -f -s /usr/bin/podman.orig /home/tooling/.local/bin/podman
+fi
+
+#############################################################################
+# Stow: If persistUserHome is enabled, then the contents of /home/user/
+# will be mounted by a PVC and overwritten. In this case, we use stow to
+# create symbolic links from /home/tooling/ -> /home/user/.
+# Required for https://github.com/eclipse/che/issues/22412
+#############################################################################
+
+# /home/user/ will be mounted to by a PVC if persistUserHome is enabled
+# We need to override the `set -e` from this script by ensuring the mountpoint command returns 0,
+# but we also need to capture the exit code of mountpoint
+HOME_USER_MOUNTED=0
+mountpoint -q /home/user/ || HOME_USER_MOUNTED=$?
+
+# This file will be created after stowing, to guard from executing stow everytime the container is started
+STOW_COMPLETE=/home/user/.stow_completed
+
+if [ $HOME_USER_MOUNTED -eq 0 ] && [ ! -f $STOW_COMPLETE ]; then
+    # Create symbolic links from /home/tooling/ -> /home/user/
+    stow . -t /home/user/ -d /home/tooling/ --no-folding -v 2 > /tmp/stow.log 2>&1
+    # Vim does not permit .viminfo to be a symbolic link for security reasons, so manually copy it
+    cp --no-clobber /home/tooling/.viminfo /home/user/.viminfo
+    # We have to restore bash-related files back onto /home/user/ (since they will have been overwritten by the PVC)
+    # but we don't want them to be symbolic links (so that they persist on the PVC)
+    cp --no-clobber /home/tooling/.bashrc /home/user/.bashrc
+    cp --no-clobber /home/tooling/.bash_profile /home/user/.bash_profile
+    touch $STOW_COMPLETE
 fi
 
 exec "$@"
