e5c95c719c6e183e83a09ec808597788dfd93a25
RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>
2023-12-07 16:19:03 -0500

    fix typo and add some better checking so build fails if VSIX_RIPGREP_PREBUILT and VSCODE_RIPGREP versions are not defined
    Signed-off-by: RHDH Build (rhdh-bot) <rhdh-bot@redhat.com>

--- devspaces-code/build/scripts/sync-builtins.sh
+++ devspaces-code/build/scripts/sync-builtins.sh
@@ -58,8 +58,7 @@ addRipGrepLibrary() {
   RG_VERSION=$1
   RG_SHA=""
   RG_SOURCE_SHA=""
-  for rcm_version in "${AVAILABLE_RG_VERSIONS[@]}"
-  do
+  for rcm_version in "${AVAILABLE_RG_VERSIONS[@]}"; do
     if [[ "${rcm_version}" == "${RG_VERSION}" ]]; then
       echo "[info] found required ripgrep-prebuilt ${RG_VERSION} for ${platform} platform"
       RG_SHA=$(jq ".\"${rcm_version}\".\"${platform}\".sha" /tmp/manifest.json)
@@ -77,9 +76,9 @@ addRipGrepLibrary() {
   esac
   RG_REMOTE_LOCATION="$BASE_URL/common/v${RG_VERSION}/ripgrep-v${RG_VERSION}-${RG_ARCH_SUFFIX}.tar.gz"
   if [[ $RG_SHA == "" ]]; then
-    echo "[info] not found required ripgrep-prebuilt ${RG_VERSION} for ${platform} platform"
-    echo "[info] downloading and publishing to rcm-tools now"
-    if [[ -f /tmp/uploadBuildRequirementsToSpmmUtil.sh ]]; then
+    echo "[info] Did not find required ripgrep-prebuilt ${RG_VERSION} for ${platform} platform"
+    echo "[info] Download and push to rcm-tools: "
+    if [[ ! -f /tmp/uploadBuildRequirementsToSpmmUtil.sh ]]; then
       curl -sSL https://raw.githubusercontent.com/redhat-developer/devspaces/${SCRIPTS_BRANCH}/product/uploadBuildRequirementsToSpmmUtil.sh -o /tmp/uploadBuildRequirementsToSpmmUtil.sh
       chmod +x /tmp/uploadBuildRequirementsToSpmmUtil.sh
     fi
@@ -111,18 +110,28 @@ addRipGrepLibrary() {
 - url: $RG_REMOTE_LOCATION
   sha256: $RG_SHA
   source-url: https://github.com/microsoft/ripgrep-prebuilt/archive/refs/tags/v${RG_VERSION}.tar.gz
-  source-sha256: $RG_SOURCE_SHA" >> ${TARGETDIR}/fetch-artifacts-url.yaml
+  source-sha256: $RG_SOURCE_SHA" >> "${TARGETDIR}/fetch-artifacts-url.yaml"
 }
 
 addRipGrepToYaml() {
   #fetch post-install script for vscode ripgrep extension, in which we find the required version of ripgrep
-  VSCODE_RIPGREP_VERSION=$(yarn list --frozen-lockfile --pattern vscode/ripgrep --depth=0 --flat | grep -o -E @vscode/ripgrep@\\S* | cut -d '@' -f 3)
+  set -x 
+  VSCODE_RIPGREP_VERSION=$(cd "$TARGETDIR"/code; yarn list --frozen-lockfile --pattern vscode/ripgrep --depth=0 --flat | grep -o -E @vscode/ripgrep@\\S* | cut -d '@' -f 3)
   # cache directory in brew.Dockerfile must be updated according to this version
   sed -r -e "s|(COPY artifacts/ripgrep-\*.tar.gz /tmp/vscode-ripgrep-cache-).*|\1${VSCODE_RIPGREP_VERSION}/|" -i "${TARGETDIR}/build/dockerfiles/brew.Dockerfile"
   POST_INSTALL_SCRIPT=$(curl -sSL https://raw.githubusercontent.com/microsoft/vscode-ripgrep/v${VSCODE_RIPGREP_VERSION}/lib/postinstall.js)
   VSIX_RIPGREP_PREBUILT_VERSION=$(echo "${POST_INSTALL_SCRIPT}" | grep "const VERSION" | cut -d"'" -f 2 | tr -d v )
   VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION=$(echo "${POST_INSTALL_SCRIPT}" | grep "const MULTI_ARCH_LINUX_VERSION" | cut -d"'" -f 2 | tr -d v )
-  echo "[info] vscode-ripgrep dependency version: ${VSCODE_RIPGREP_VERSION} depends on ripgrep-prebuilt ${VSIX_RIPGREP_PREBUILT_VERSION}, multiarch - ${VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION}"
+  if [[ $VSCODE_RIPGREP_VERSION ]] && [[ $VSIX_RIPGREP_PREBUILT_VERSION ]] && [[ $VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION ]]; then 
+    echo "[info] vscode-ripgrep ${VSCODE_RIPGREP_VERSION} depends on ripgrep-prebuilt ${VSIX_RIPGREP_PREBUILT_VERSION} and multiarch ${VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION}"
+  else
+    echo "[ERROR] One or more ripgrep version not defined! Must exit!
+        VSCODE_RIPGREP_VERSION: $VSCODE_RIPGREP_VERSION
+        VSIX_RIPGREP_PREBUILT_VERSION: $VSIX_RIPGREP_PREBUILT_VERSION
+        VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION: $ VSIX_RIPGREP_PREBUILT_MULTIARCH_VERSION
+"; exit 2
+  fi
+
   # collect all current available versions on rcm-tools
   curl -sSL https://download.devel.redhat.com/rcm-guest/staging/devspaces/build-requirements/common/ripgrep-multiarch/manifest.json -o /tmp/manifest.json
   if ! jq empty /tmp/manifest.json; then
@@ -180,7 +189,7 @@ addVscodePluginsToYaml () {
     AVAILABLE_VSIX_VERSION_NO_PREFIX=${AVAILABLE_VSIX_VERSION#v}
     REQUIRED_VSIX_VERSION=$(jq -r ".builtInExtensions[] | select( .name==\"${PLUGIN}\") | .version" "$TARGETDIR"/code/product.json)
     echo "[info] looking for plugin \"${PLUGIN}\", version ${REQUIRED_VSIX_VERSION}" 
-    if [[ $REQUIRED_VSIX_VERSION == ${AVAILABLE_VSIX_VERSION_NO_PREFIX} ]]; then
+    if [[ $REQUIRED_VSIX_VERSION == "${AVAILABLE_VSIX_VERSION_NO_PREFIX}" ]]; then
       echo "[info] found required vsix extension - ${PLUGIN}, version ${REQUIRED_VSIX_VERSION}"
       PLUGIN_SHA=$(jq -r ".Plugins[\"$PLUGIN\"][\"vsix\"]" "/tmp/plugin-manifest.json")
       SOURCE_SHA=$(jq -r ".Plugins[\"$PLUGIN\"][\"source\"]" "/tmp/plugin-manifest.json")
