fe5bbfdcbae6257fe5617717c33bdf0fd5622d39
devstudio-release <nickboldt+devstudio-release@gmail.com>
2024-08-14 04:48:05 -0400

    ci: [sync] Update from eclipse-che/che-dashboard @ 853c7b
    Signed-off-by: devstudio-release <nickboldt+devstudio-release@gmail.com>

--- devspaces-dashboard/build/dockerfiles/entrypoint.sh
+++ devspaces-dashboard/build/dockerfiles/entrypoint.sh
@@ -11,7 +11,21 @@
 set -e
 
 echo 'Starting Dashboard backend server...'
-find /public/dashboard/devfile-registry/air-gap -type f \( -name "*.json" -o -name "*.yaml" \) -exec sed -i 's|CHE_DASHBOARD_INTERNAL_URL|'${CHE_DASHBOARD_INTERNAL_URL}'|g' {} \;
+
+# Update internal dashboard url
+SAMPLES_DIR=/public/dashboard/devfile-registry/air-gap
+find "${SAMPLES_DIR}" -type f \( -name "index.json" -o -name "*devfile.yaml" \) -exec sed -i 's|CHE_DASHBOARD_INTERNAL_URL|'${CHE_DASHBOARD_INTERNAL_URL}'|g' {} \;
+
+# Update the related images in the devfile
+RELATED_IMAGES_ENVS=$(env | grep RELATED_IMAGE_sample_encoded_ || true)
+if [ -n "${RELATED_IMAGES_ENVS}" ]; then
+  for RELATED_IMAGE_ENV in ${RELATED_IMAGES_ENVS}; do
+    OLD_IMAGE=$(echo "${RELATED_IMAGE_ENV}" | cut -d'=' -f1 | sed 's|____|=|g' | sed 's|RELATED_IMAGE_sample_encoded_||g' | base64 -d)
+    NEW_IMAGE=$(echo "${RELATED_IMAGE_ENV}" | cut -d'=' -f2)
+    find "${SAMPLES_DIR}" -type f -name "*devfile.yaml" -exec sed -i 's|'"${OLD_IMAGE}"'|'"${NEW_IMAGE}"'|g' {} \;
+  done
+fi
+
 start_server="node /backend/server/backend.js --publicFolder /public"
 $start_server &
 wait
